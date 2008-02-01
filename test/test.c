dc66a07cc9adbc89f8fe20956207a883520dfdf7
halleyscometsw <halleyscometsw@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2008-02-01 02:41:08 +0000

    Fixed test reporting of sample count
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@5 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- test/test.c
+++ test/test.c
@@ -26,7 +26,7 @@ int main(int argc, char ** argv) {
         printf("loop start: %d samples (%lf seconds)\n",s->loop_start_sample,(double)s->loop_start_sample/s->sample_rate);
         printf("loop end: %d samples (%lf seconds)\n",s->loop_end_sample,(double)s->loop_end_sample/s->sample_rate);
     }
-    printf("file total samples %d (%lf seconds)\n",s->num_samples);
+    printf("file total samples %d (%lf seconds)\n",s->num_samples,(double)s->num_samples/s->sample_rate);
 
     len = get_vgmstream_play_samples(2.0,10.0,s);
     printf("samples to play %d (%lf seconds)\n",len,(double)len/s->sample_rate);
