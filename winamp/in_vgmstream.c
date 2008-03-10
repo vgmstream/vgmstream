ac832cce80273c559010f653e6840c9cb9671fe8
halleyscometsw <halleyscometsw@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2008-03-10 22:58:31 +0000

    initial winamp plugin
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@41 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

---
+++ winamp/in_vgmstream.c
@@ -0,0 +1,267 @@
+/* Winamp plugin interface for vgmstream */
+/* Largely copied from: */
+/*
+** Example Winamp .RAW input plug-in
+** Copyright (c) 1998, Justin Frankel/Nullsoft Inc.
+*/
+
+#include <windows.h>
+#include <stdio.h>
+
+#include "../src/vgmstream.h"
+#include "in2.h"
+
+#define PLUGIN_DESCRIPTION "vgmstream plugin ($Revision$, $Date$)"
+
+/* post when playback stops */
+#define WM_WA_MPEG_EOF WM_USER+2
+
+In_Module input_module; /* the input module, declared at the bottom of this file */
+DWORD WINAPI __stdcall decode(void *arg);
+
+char lastfn[MAX_PATH+1] = {0}; /* name of the currently playing file */
+short sample_buffer[576*2*2]; /* 576 16-bit samples, stereo, possibly doubled in size for DSP */
+
+/* hardcode these now, TODO will be configurable later */
+const double fade_seconds = 10.0;
+const double loop_count = 2.0;
+
+VGMSTREAM * vgmstream = NULL;
+HANDLE decode_thread_handle = INVALID_HANDLE_VALUE;
+int paused = 0;
+int decode_abort = 0;
+int decode_pos_ms = 0;
+int decode_pos_samples = 0;
+int stream_length_samples = 0;
+int fade_samples = 0;
+
+/* stubs, we don't do anything fancy yet */
+void config(HWND hwndParent) {}
+void about(HWND hwndParent) {}
+void init() {}
+void quit() {}
+
+/* we don't recognize protocols */
+int isourfile(char *fn) { return 0; }
+
+/* request to start playing a file */
+int play(char *fn)
+{
+    int max_latency;
+
+
+    /* don't lose a pointer! */
+    if (vgmstream) {
+        /* TODO: this should either pop up an error box or close the file */
+        return 1;
+    }
+    /* open the stream, set up */
+    vgmstream = init_vgmstream(fn);
+    /* were we able to open it? */
+    if (!vgmstream) {
+        return 1;
+    }
+    /* will we be able to play it? */
+    if (vgmstream->channels <= 0 || vgmstream->channels > 2) {
+        /* TODO: > 2 channels is not unheard of, we should probably complain */
+        close_vgmstream(vgmstream);
+        vgmstream=NULL;
+        return 1;
+    }
+
+    /* Remember that name, friends! */
+    strncpy(lastfn,fn,MAX_PATH);
+
+    /* open the output plugin */
+    max_latency = input_module.outMod->Open(vgmstream->sample_rate,vgmstream->channels,
+            16, 0, 0);
+    /* were we able to open it? */
+    if (max_latency < 0) {
+        close_vgmstream(vgmstream);
+        vgmstream=NULL;
+    }
+
+    /* Set info display */
+    /* TODO: actual bitrate */
+    input_module.SetInfo(100,vgmstream->sample_rate,vgmstream->channels,1);
+
+    /* setup visualization */
+    input_module.SAVSAInit(max_latency,vgmstream->sample_rate);
+    input_module.VSASetInfo(vgmstream->sample_rate,vgmstream->channels);
+
+    decode_abort = 0;
+    decode_pos_ms = 0;
+    decode_pos_samples = 0;
+    paused = 0;
+    stream_length_samples = get_vgmstream_play_samples(loop_count,fade_seconds,vgmstream);
+    fade_samples = fade_seconds * vgmstream->sample_rate;
+
+    decode_thread_handle = CreateThread(
+            NULL,   /* handle cannot be inherited */
+            0,      /* stack size, 0=default */
+            decode, /* thread start routine */
+            NULL,   /* no parameter to start routine */
+            0,      /* run thread immediately */
+            NULL);  /* don't keep track of the thread id */
+
+    return 0;
+}
+
+/* pausing... */
+void pause() { paused=1; input_module.outMod->Pause(1); }
+void unpause() {paused=0; input_module.outMod->Pause(0); }
+int ispaused() { return paused; }
+
+/* stop playback */
+void stop() {
+    if (decode_thread_handle != INVALID_HANDLE_VALUE) {
+        decode_abort=1;
+
+        /* arbitrary wait length */
+        if (WaitForSingleObject(decode_thread_handle,1000) == WAIT_TIMEOUT) {
+            /* TODO: error? */
+            TerminateThread(decode_thread_handle,0);
+        }
+        CloseHandle(decode_thread_handle);
+        decode_thread_handle = INVALID_HANDLE_VALUE;
+    }
+
+    if (vgmstream) {
+        close_vgmstream(vgmstream);
+        vgmstream=NULL;
+    }
+
+    input_module.outMod->Close();
+    input_module.SAVSADeInit();
+}
+
+/* get current stream length */
+int getlength() {
+    return stream_length_samples*1000L/vgmstream->sample_rate;
+}
+
+/* get current output time */
+int getoutputtime() {
+    return decode_pos_ms+(input_module.outMod->GetOutputTime()-input_module.outMod->GetWrittenTime());
+}
+
+/* seek */
+void setoutputtime() {
+    /* TODO: seeking */
+}
+
+/* pass these commands through */
+void setvolume(int volume) { input_module.outMod->SetVolume(volume); }
+void setpan(int pan) { input_module.outMod->SetPan(pan); }
+
+/* display information */
+int infoDlg(char *fn, HWND hwnd) {
+    /* TODO: info box */
+    return 0;
+}
+
+/* retrieve information on this or possibly another file */
+void getfileinfo(char *filename, char *title, int *length_in_ms) {
+    /* TODO */
+}
+
+/* nothin' */
+void eq_set(int on, char data[10], int preamp) {}
+
+/* the decode thread */
+DWORD WINAPI __stdcall decode(void *arg) {
+    while (!decode_abort) {
+
+        int samples_to_do;
+        int l;
+        if (decode_pos_samples+576>stream_length_samples)
+            samples_to_do=stream_length_samples-decode_pos_samples;
+        else
+            samples_to_do=576;
+
+        l = (samples_to_do*vgmstream->channels*2)<<(input_module.dsp_isactive()?1:0);
+
+        if (samples_to_do == 0) {
+            input_module.outMod->CanWrite();    /* ? */
+            if (!input_module.outMod->IsPlaying()) {
+                PostMessage(input_module.hMainWindow,   /* message dest */
+                        WM_WA_MPEG_EOF,     /* message id */
+                        0,0);   /* no parameters */
+                return 0;
+            }
+            Sleep(10);
+        }
+        else if (input_module.outMod->CanWrite() >= l) {
+            /* let vgmstream do its thing */
+            render_vgmstream(sample_buffer,samples_to_do,vgmstream);
+
+            /* fade! */
+            if (vgmstream->loop_flag && fade_samples > 0) {
+                int samples_into_fade = decode_pos_samples - (stream_length_samples - fade_samples);
+                if (samples_into_fade + samples_to_do > 0) {
+                    int j,k;
+                    for (j=0;j<samples_to_do;j++,samples_into_fade++) {
+                        if (samples_into_fade > 0) {
+                            double fadedness = (double)(fade_samples-samples_into_fade)/fade_samples;
+                            for (k=0;k<vgmstream->channels;k++) {
+                                sample_buffer[j*vgmstream->channels+k] =
+                                sample_buffer[j*vgmstream->channels+k]*fadedness;
+                            }
+                        }
+                    }
+                }
+            }
+
+            input_module.SAAddPCMData((char*)sample_buffer,vgmstream->channels,16,decode_pos_ms);
+            input_module.VSAAddPCMData((char*)sample_buffer,vgmstream->channels,16,decode_pos_ms);
+            decode_pos_samples+=samples_to_do;
+            decode_pos_ms=decode_pos_samples*1000L/vgmstream->sample_rate;
+            if (input_module.dsp_isactive())
+                l =input_module.dsp_dosamples(sample_buffer,samples_to_do,16,vgmstream->channels,vgmstream->sample_rate) * 
+                    2 * vgmstream->channels;
+
+            input_module.outMod->Write((char*)sample_buffer,l);
+        }   /* if we can write enough */
+        else Sleep(20);
+    }   /* main loop */
+    return 0;
+}
+
+In_Module input_module = 
+{
+    IN_VER,
+    PLUGIN_DESCRIPTION,
+    0,  // hMainWindow
+    0,  // hDllInstance
+    "ADX\0ADX Audio File\0",
+    1, // is_seekable
+    1, // uses output
+    config,
+    about,
+    init,
+    quit,
+    getfileinfo,
+    infoDlg,
+    isourfile,
+    play,
+    pause,
+    unpause,
+    ispaused,
+    stop,
+    getlength,
+    getoutputtime,
+    setoutputtime,
+    setvolume,
+    setpan,
+    0,0,0,0,0,0,0,0,0, // vis stuff
+    0,0, // dsp
+    eq_set,
+    NULL,       // setinfo
+    0 // out_mod
+};
+
+__declspec( dllexport ) In_Module * winampGetInModule2()
+{
+    return &input_module;
+}
+
