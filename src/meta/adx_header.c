7c2e8a32d1573b61839d86a36fba949897055135
halleyscometsw <halleyscometsw@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2008-05-10 03:56:39 +0000

    Improved ADX header stuff. Probably of no practical use.
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@94 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/adx_header.c
+++ src/meta/adx_header.c
@@ -8,9 +8,9 @@
 VGMSTREAM * init_vgmstream_adx(const char * const filename) {
     VGMSTREAM * vgmstream = NULL;
     STREAMFILE * infile = NULL;
-    off_t copyright_offset;
+    off_t stream_offset;
     size_t filesize;
-    uint32_t version_signature;
+    uint16_t version_signature;
     int loop_flag=0;
     int channel_count;
     int32_t loop_start_offset=0;
@@ -19,6 +19,7 @@ VGMSTREAM * init_vgmstream_adx(const char * const filename) {
     int32_t loop_end_sample=0;
     meta_t header_type;
     int16_t coef1, coef2;
+    uint16_t cutoff;
 
     /* check extension, case insensitive */
     if (strcasecmp("adx",filename_extension(filename))) goto fail;
@@ -32,45 +33,58 @@ VGMSTREAM * init_vgmstream_adx(const char * const filename) {
     /* check first 2 bytes */
     if ((uint16_t)read_16bitBE(0,infile)!=0x8000) goto fail;
 
-    /* get copyright/stream offset, check */
-    copyright_offset = (uint16_t)read_16bitBE(2,infile);
-    if ((uint32_t)read_32bitBE(copyright_offset,infile)!=0x29435249 || /* ")CRI" */
-            (uint16_t)read_16bitBE(copyright_offset-2,infile)!=0x2863) /* "(c" */
-        goto fail;
+    /* get stream offset, check for CRI signature just before */
+    stream_offset = (uint16_t)read_16bitBE(2,infile) + 4;
+    if ((uint16_t)read_16bitBE(stream_offset-6,infile)!=0x2863 ||/* "(c" */
+        (uint32_t)read_32bitBE(stream_offset-4,infile)!=0x29435249 /* ")CRI" */
+       ) goto fail;
+
+    /* check for encoding type */
+    /* 2 is for some unknown fixed filter, 3 is standard ADX, 4 is
+     * ADX with exponential scale, 0x11 is AHX */
+    if (read_8bit(4,infile) != 3) goto fail;
+
+    /* check for frame size (only 18 is supported at the moment) */
+    if (read_8bit(5,infile) != 18) goto fail;
+
+    /* check for bits per sample? (only 4 makes sense for ADX) */
+    if (read_8bit(6,infile) != 4) goto fail;
 
     /* check version signature, read loop info */
-    /* TODO: at some point will we want to support encrypted ADXs? */
-    version_signature = read_32bitBE(0x10,infile);
-    if (version_signature == 0x01F40300) {      /* type 03 */
+    version_signature = read_16bitBE(0x12,infile);
+    if (version_signature == 0x0300) {      /* type 03 */
         header_type = meta_ADX_03;
-        if (copyright_offset-2 >= 0x2c) {   /* enough space for loop info? */
+        if (stream_offset-6 >= 0x2c) {   /* enough space for loop info? */
             loop_flag = (read_32bitBE(0x18,infile) != 0);
             loop_start_sample = read_32bitBE(0x1c,infile);
             loop_start_offset = read_32bitBE(0x20,infile);
             loop_end_sample = read_32bitBE(0x24,infile);
             loop_end_offset = read_32bitBE(0x28,infile);
         }
-    } else if (version_signature == 0x01F40400) {
+    } else if (version_signature == 0x0400) {
 
-		off_t	ainf_info_length=0;
+        off_t	ainf_info_length=0;
 
-		if((uint32_t)read_32bitBE(0x24,infile)==0x41494E46) /* AINF Header */
-			ainf_info_length = (off_t)read_32bitBE(0x28,infile);
+        if((uint32_t)read_32bitBE(0x24,infile)==0x41494E46) /* AINF Header */
+            ainf_info_length = (off_t)read_32bitBE(0x28,infile);
 
-		header_type = meta_ADX_04;
-        if (copyright_offset-ainf_info_length-2 >= 0x38) {   /* enough space for loop info? */
+        header_type = meta_ADX_04;
+        if (stream_offset-ainf_info_length-6 >= 0x38) {   /* enough space for loop info? */
             loop_flag = (read_32bitBE(0x24,infile) != 0);
             loop_start_sample = read_32bitBE(0x28,infile);
             loop_start_offset = read_32bitBE(0x2c,infile);
             loop_end_sample = read_32bitBE(0x30,infile);
             loop_end_offset = read_32bitBE(0x34,infile);
         }
-	} else if (version_signature == 0x01F40500) {			 /* found in some SFD : Buggy Heat */
-		header_type = meta_ADX_05;
+    } else if (version_signature == 0x0500) {			 /* found in some SFD : Buggy Heat, appears to have no loop */
+        header_type = meta_ADX_05;
     } else goto fail;   /* not a known/supported version signature */
 
     /* At this point we almost certainly have an ADX file,
      * so let's build the VGMSTREAM. */
+
+    /* high-pass cutoff frequency, always 500 that I've seen */
+    cutoff = (uint16_t)read_16bitBE(0x10,infile);
 
     channel_count = read_8bit(7,infile);
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
@@ -98,7 +112,7 @@ VGMSTREAM * init_vgmstream_adx(const char * const filename) {
     {
         double x,y,z,a,b,c;
 
-        x = 500;
+        x = cutoff;
         y = vgmstream->sample_rate;
         z = cos(2.0*M_PI*x/y);
 
@@ -118,7 +132,7 @@ VGMSTREAM * init_vgmstream_adx(const char * const filename) {
 
             vgmstream->ch[i].channel_start_offset=
                 vgmstream->ch[i].offset=
-                copyright_offset+4+18*i;
+                stream_offset+18*i;
 
             vgmstream->ch[i].adpcm_coef[0] = coef1;
             vgmstream->ch[i].adpcm_coef[1] = coef2;
