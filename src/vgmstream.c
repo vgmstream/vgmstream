573887a70d94060b9b0697cd9e166484bea1d066
halleyscometsw <halleyscometsw@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2008-12-10 04:09:41 +0000

    hey guys stop breaking the build
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@505 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/vgmstream.c
+++ src/vgmstream.c
@@ -132,7 +132,6 @@ VGMSTREAM * (*init_vgmstream_fcns[])(STREAMFILE *streamFile) = {
 	init_vgmstream_kraw,
 	init_vgmstream_ps2_omu,
 	init_vgmstream_ps2_xa2,
-	init_vgmstream_ss_stream,
 	init_vgmstream_idsp,
 	init_vgmstream_idsp2,
 	init_vgmstream_ngc_ymf,
