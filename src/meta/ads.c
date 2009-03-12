266e6573a74d3faf94e649f3f6bf0411313eef12
manakoAT <manakoAT@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2009-03-12 09:27:11 +0000

    ads updated (xbox, mono files only)
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@596 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ads.c
+++ src/meta/ads.c
@@ -6,8 +6,9 @@ VGMSTREAM * init_vgmstream_ads(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
     char filename[260];
     off_t start_offset;
-	int loop_flag;
-	int channel_count;
+    int loop_flag;
+    int channel_count;
+    int identifer_byte;
 
     /* check extension, case insensitive */
     streamFile->get_name(streamFile,filename,sizeof(filename));
@@ -20,40 +21,79 @@ VGMSTREAM * init_vgmstream_ads(STREAMFILE *streamFile) {
     /* check dbSS Header */
     if (read_32bitBE(0x20,streamFile) != 0x64625353)
         goto fail;
-
+    
     loop_flag = 1;
     channel_count = read_32bitBE(0x10,streamFile);
 
-	/* build the VGMSTREAM */
+    if (channel_count > 0x2)
+        goto fail;
+
+    /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-	/* fill in the vital statistics */
-    start_offset = 0xE8;
-	vgmstream->channels = channel_count;
-    vgmstream->sample_rate = read_32bitBE(0x0c,streamFile);
-    vgmstream->coding_type = coding_NGC_DSP;
-    vgmstream->num_samples = read_32bitBE(0x28,streamFile);
-    if (loop_flag) {
-        vgmstream->loop_start_sample = 0;
-        vgmstream->loop_end_sample = vgmstream->num_samples;
-    }
+    /* fill in the vital statistics */
+    identifer_byte = read_32bitBE(0x08,streamFile);
+    switch (identifer_byte) {
+        case 0x00000020:
+            start_offset = 0xE8;
+            vgmstream->channels = channel_count;
+            vgmstream->sample_rate = read_32bitBE(0x0c,streamFile);
+            vgmstream->coding_type = coding_NGC_DSP;
+            vgmstream->num_samples = read_32bitBE(0x28,streamFile);
+        if (loop_flag) {
+            vgmstream->loop_start_sample = 0;
+            vgmstream->loop_end_sample = vgmstream->num_samples;
+        }
+        
+        if (channel_count == 1){
+            vgmstream->layout_type = layout_none;
+        } else if (channel_count == 2){
+            vgmstream->layout_type = layout_interleave;
+            vgmstream->interleave_block_size = read_32bitBE(0x14,streamFile);
+        }
+    break;
+        case 0x00000021:
+            start_offset = 0x28;
+            vgmstream->channels = channel_count;
+            vgmstream->sample_rate = read_32bitBE(0x0c,streamFile);
+            vgmstream->coding_type = coding_XBOX;
+            vgmstream->num_samples = read_32bitBE(0x24,streamFile);
+            vgmstream->layout_type = layout_none;
+            vgmstream->interleave_block_size = 0x24;
+        if (loop_flag) {
+            vgmstream->loop_start_sample = 0;
+            vgmstream->loop_end_sample = vgmstream->num_samples;
+        }
+        break;
+    default:
+goto fail;
+}
 
-    vgmstream->layout_type = layout_interleave;
-    vgmstream->interleave_block_size = 0x20;
     vgmstream->meta_type = meta_ADS;
 
-    if (vgmstream->coding_type == coding_NGC_DSP) {
+        {
         int i;
-        for (i=0;i<16;i++) {
+        for (i=0;i<16;i++)
             vgmstream->ch[0].adpcm_coef[i] = read_16bitBE(0x44+i*2,streamFile);
+        if (channel_count == 2) {
+        for (i=0;i<16;i++)
+            vgmstream->ch[1].adpcm_coef[i] = read_16bitBE(0xA4+i*2,streamFile);
+    }
         }
-        if (vgmstream->channels) {
+
+
+    /* open the file for reading */
+    if (vgmstream->coding_type == coding_NGC_DSP) {
+        int i,c;
+        for (c=0;c<channel_count;c++) {
             for (i=0;i<16;i++) {
-                vgmstream->ch[1].adpcm_coef[i] = read_16bitBE(0xA4+i*2,streamFile);
+                vgmstream->ch[c].adpcm_coef[i] =
+                    read_16bitBE(0x44+c*0x60 +i*2,streamFile);
             }
         }
     }
+
     /* open the file for reading */
     {
         int i;
@@ -63,12 +103,17 @@ VGMSTREAM * init_vgmstream_ads(STREAMFILE *streamFile) {
         for (i=0;i<channel_count;i++) {
             vgmstream->ch[i].streamfile = file;
 
-            vgmstream->ch[i].channel_start_offset=
-                vgmstream->ch[i].offset=start_offset+
-                vgmstream->interleave_block_size*i;
+            
+            if (vgmstream->coding_type == coding_XBOX) {
+                /* xbox interleaving is a little odd */
+                vgmstream->ch[i].channel_start_offset=start_offset;
+            } else {
+                vgmstream->ch[i].channel_start_offset=
+                    start_offset+vgmstream->interleave_block_size*i;
+            }
+            vgmstream->ch[i].offset = vgmstream->ch[i].channel_start_offset;
 
         }
-
     }
 
     return vgmstream;
