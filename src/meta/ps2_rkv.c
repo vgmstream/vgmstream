be5c926321cecccddff705606f103cfff4763269
fastelbja <fastelbja@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2009-09-09 20:57:52 +0000

    fix RKV support
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@696 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ps2_rkv.c
+++ src/meta/ps2_rkv.c
@@ -5,7 +5,7 @@
 VGMSTREAM * init_vgmstream_ps2_rkv(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
     char filename[260];
-    off_t start_offset;
+    off_t start_offset=0;
     int loop_flag;
 	int channel_count;
 
@@ -13,32 +13,43 @@ VGMSTREAM * init_vgmstream_ps2_rkv(STREAMFILE *streamFile) {
     streamFile->get_name(streamFile,filename,sizeof(filename));
     if (strcasecmp("rkv",filename_extension(filename))) goto fail;
 
-    /* check header */
-    if (read_32bitBE(0x00,streamFile) != 0x00000000) /* "\0000" */
-        goto fail;
+	// Some RKV got info @ offset 0
+	// Some other @ offset 4
+	if(read_32bitLE(0,streamFile)==0) 
+		start_offset=4;
 
-	loop_flag = (read_32bitLE(0x10,streamFile)!=0);
-    channel_count = 2;
+	loop_flag = (read_32bitLE(start_offset+4,streamFile)!=0xFFFFFFFF);
+    channel_count = read_32bitLE(start_offset+0x0c,streamFile)+1;
     
 	/* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
 	/* fill in the vital statistics */
-	start_offset = 0x800;
 	vgmstream->channels = channel_count;
-	vgmstream->sample_rate = read_32bitLE(0x04,streamFile);
+	vgmstream->sample_rate = read_32bitLE(start_offset,streamFile);
     vgmstream->coding_type = coding_PSX;
-    vgmstream->num_samples = read_32bitLE(0x0C,streamFile);
-    if (loop_flag) {
-        vgmstream->loop_start_sample = read_32bitLE(0x10,streamFile)*28/16/channel_count;
-        vgmstream->loop_end_sample = read_32bitLE(0x0C,streamFile);
+
+	// sometimes sample count is not set on the header 
+    vgmstream->num_samples = (get_streamfile_size(streamFile)-0x800)/16*28/channel_count;
+    
+	if (loop_flag) {
+        vgmstream->loop_start_sample = read_32bitLE(start_offset+4,streamFile);
+        vgmstream->loop_end_sample = read_32bitLE(start_offset+8,streamFile);
     }
 
-    vgmstream->layout_type = layout_interleave;
-    vgmstream->interleave_block_size = 0x400;
-    vgmstream->meta_type = meta_PS2_RKV;
+	start_offset = 0x800;
 
+	if((get_streamfile_size(streamFile)-0x800)%0x400) 
+	{
+		vgmstream->layout_type = layout_interleave_shortblock;
+		vgmstream->interleave_smallblock_size=((get_streamfile_size(streamFile)-0x800)%0x400)/channel_count;
+	} else {
+		vgmstream->layout_type = layout_interleave;
+	}
+
+	vgmstream->interleave_block_size = 0x400;
+	vgmstream->meta_type = meta_PS2_RKV;
     /* open the file for reading */
     {
         int i;
