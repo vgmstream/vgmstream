87a52ca673a87310b1aa7cbf7463ba05317589e4
bxaimc <bxaimc@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2010-05-09 19:46:31 +0000

    Fixed MIHB sample calculation
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@793 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ps2_mihb.c
+++ src/meta/ps2_mihb.c
@@ -31,10 +31,10 @@ VGMSTREAM * init_vgmstream_ps2_mihb(STREAMFILE *streamFile) {
 	vgmstream->channels = channel_count;
     vgmstream->sample_rate = read_32bitLE(0x0C,streamFile);
     vgmstream->coding_type = coding_PSX;
-    vgmstream->num_samples = ((read_32bitLE(0x10,streamFile))*mib_blocks)*28/16/channel_count;
+    vgmstream->num_samples = ((read_32bitLE(0x10,streamFile))*mib_blocks)*28/16;
     if (loop_flag) {
         vgmstream->loop_start_sample = 0;
-        vgmstream->loop_end_sample = ((read_32bitLE(0x10,streamFile))*mib_blocks)*28/16/channel_count;
+        vgmstream->loop_end_sample = ((read_32bitLE(0x10,streamFile))*mib_blocks)*28/16;
     }
 
     
