b17c0b867ecf20fd10e71be46ef22c86e5697749
snakemeat <snakemeat@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2010-05-31 17:23:15 +0000

    Update PS2 ADS with special check for stream size used in True Fortune.
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@799 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ps2_ads.c
+++ src/meta/ps2_ads.c
@@ -39,7 +39,8 @@ VGMSTREAM * init_vgmstream_ps2_ads(STREAMFILE *streamFile) {
 	/* so I changed it to adapt the stream size if that's the case */
 	/* instead of failing playing them at all*/
 	streamSize = read_32bitLE(0x24,streamFile);
-    if (get_streamfile_size(streamFile)<(size_t)( streamSize+ 0x28))
+    
+	if (get_streamfile_size(streamFile) < (size_t)(streamSize + 0x28))
 	{
 		streamSize = get_streamfile_size(streamFile) - 0x28;
 	}
@@ -62,7 +63,8 @@ VGMSTREAM * init_vgmstream_ps2_ads(STREAMFILE *streamFile) {
     vgmstream->num_samples = ((streamSize-0x40)/16*28)/vgmstream->channels;
 
 	/* SS2 container with RAW Interleaved PCM */
-    if (read_32bitLE(0x08,streamFile)!=0x10) {
+    if (read_32bitLE(0x08,streamFile)!=0x10) 
+	{
 
         vgmstream->coding_type=coding_PCM16LE;
         vgmstream->num_samples = streamSize/2/vgmstream->channels;
@@ -109,17 +111,31 @@ VGMSTREAM * init_vgmstream_ps2_ads(STREAMFILE *streamFile) {
     /* don't know why, but it does happen, in ps2 too :( */
     if (vgmstream->loop_end_sample > vgmstream->num_samples)
         vgmstream->loop_end_sample = vgmstream->num_samples;
+	{
+		start_offset=0x28;
+	}
 
-	start_offset=0x28;
-
-	// Hack for files with start_offset = 0x800
-	if(get_streamfile_size(streamFile)-read_32bitLE(0x24,streamFile)>=0x800)
+		
+	if ((streamSize * 2) == (get_streamfile_size(streamFile) - 0x18))
+	{
+		// True Fortune PS2
+		streamSize = (read_32bitLE(0x24,streamFile) * 2) - 10;
+		vgmstream->num_samples = streamSize / 16 * 28 / vgmstream->channels;
+	}
+	else if(get_streamfile_size(streamFile) - read_32bitLE(0x24,streamFile) >= 0x800)
+	{
+		// Hack for files with start_offset = 0x800
 		start_offset=0x800;
+	}
 
-	if((vgmstream->coding_type == coding_PSX) && (start_offset==0x28)) {
+	if((vgmstream->coding_type == coding_PSX) && (start_offset==0x28)) 
+	{
 		start_offset=0x800;
-		for(i=0;i<0x1f6;i+=4) {
-			if(read_32bitLE(0x28+(i*4),streamFile)!=0) {
+		
+		for(i=0;i<0x1f6;i+=4) 
+		{
+			if(read_32bitLE(0x28+(i*4),streamFile)!=0) 
+			{
 				start_offset=0x28;
 				break;
 			}
@@ -130,17 +146,22 @@ VGMSTREAM * init_vgmstream_ps2_ads(STREAMFILE *streamFile) {
 	if(vgmstream->coding_type==coding_PCM16LE) 
 	{
 		check_offset=start_offset;
-		do {
+		do 
+		{
 			if(read_8bit(check_offset+1,streamFile)>7) 
 			{
 				isPCM=1;
 				break;
-			} else
+			} 
+			else
+			{
 				check_offset+=0x10;
+			}
 			
 		} while (check_offset<get_streamfile_size(streamFile));
 
-		if(!isPCM) {
+		if(!isPCM) 
+		{
 			vgmstream->num_samples=(get_streamfile_size(streamFile)-start_offset)/16*28/vgmstream->channels;
 			vgmstream->coding_type=coding_PSX;
 		}
