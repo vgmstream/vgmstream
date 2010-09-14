38507843ea57e2262129fe8429031e70efa68d23
bxaimc <bxaimc@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2010-09-14 03:01:07 +0000

    Yay! I feel smarticle! Added PCM .CPS support
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@847 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ps3_cps.c
+++ src/meta/ps3_cps.c
@@ -30,15 +30,23 @@ VGMSTREAM * init_vgmstream_ps3_cps(STREAMFILE *streamFile) {
     start_offset = read_32bitBE(0x4,streamFile);
    vgmstream->channels = channel_count;
     vgmstream->sample_rate = read_32bitBE(0x10,streamFile);
-    vgmstream->coding_type = coding_PSX;
-    vgmstream->num_samples = read_32bitBE(0xc,streamFile)*28/32;
+	if (read_32bitBE(0x20,streamFile)==0x00000000){
+		vgmstream->coding_type = coding_PCM16BE;
+	    vgmstream->num_samples = read_32bitBE(0xc,streamFile)/4;
+		vgmstream->interleave_block_size = 2;
+	}
+	else {
+		vgmstream->coding_type = coding_PSX;
+	    vgmstream->num_samples = read_32bitBE(0xc,streamFile)*28/32;
+		vgmstream->interleave_block_size = 0x10;
+	}
+
     if (loop_flag) {
         vgmstream->loop_start_sample = read_32bitBE(0x14,streamFile)*28/32;
        vgmstream->loop_end_sample = read_32bitBE(0x18,streamFile)*28/32;
     }
 
     vgmstream->layout_type = layout_interleave;
-    vgmstream->interleave_block_size = 0x10;
     vgmstream->meta_type = meta_PS3_CPS;
 
     /* open the file for reading */
