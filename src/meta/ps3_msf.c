399d570a83194d2ac5dda14b73cefe749c8647ad
manakoAT <manakoAT@51a99a44-fe44-0410-b1ba-c3e57ba2b86b>
2010-09-17 18:32:18 +0000

    fixed MPEG stuff, loops and crashing files in PS3_MSF.C
    git-svn-id: https://vgmstream.svn.sourceforge.net/svnroot/vgmstream@853 51a99a44-fe44-0410-b1ba-c3e57ba2b86b

--- src/meta/ps3_msf.c
+++ src/meta/ps3_msf.c
@@ -2,133 +2,100 @@
 #include "../coding/coding.h"
 #include "../util.h"
 
+/* MSF header */
 VGMSTREAM * init_vgmstream_ps3_msf(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
     char filename[260];
     off_t start_offset;
+    int32_t loop_start, loop_end;
     int loop_flag = 0;
-    int channel_count;
-    long sample_rate = 0;
-    long num_samples = 0;
+  	int channel_count;
+    int codec_id;
 
-#ifdef VGM_USE_MPEG
-    mpeg_codec_data *mpeg_data = NULL;
-    coding_t mpeg_coding_type = coding_MPEG1_L3;
-#endif
-
-	uint16_t mp3ID;
+//    int32_t (*read_32bit)(off_t,STREAMFILE*) = NULL;
+//    int16_t (*read_16bit)(off_t,STREAMFILE*) = NULL;
 
     /* check extension, case insensitive */
     streamFile->get_name(streamFile,filename,sizeof(filename));
     if (strcasecmp("msf",filename_extension(filename))) goto fail;
 
-    /* check header */
-//    if (read_32bitBE(0x00,streamFile) != 0x58564147) /* "XVAG" */
-//        goto fail;
+    /* MSFC */
+    if (read_32bitBE(0x0,streamFile) != 0x4D534643) goto fail;
 
+    loop_flag = (read_32bitBE(0x18,streamFile) != 0xFFFFFFFF);
+    if (loop_flag)
+    {
+      loop_start = read_32bitBE(0x18,streamFile);
+      loop_end = read_32bitBE(0x0C,streamFile);
+    }
 
-
-		channel_count = 2; // read_32bitBE(0x28,streamFile);
-		start_offset = 0x40; //read_32bitBE(0x4,streamFile);
-		//sample_rate = read_32bitBE(0x3c,streamFile);
-	  num_samples = read_32bitBE(0x1C,streamFile)/960*1152;
-
-
-
-	// MP3s ?
-	mp3ID=(uint16_t)read_16bitBE(0x40,streamFile);
-	if(mp3ID==0xFFFA) {
-#ifdef VGM_USE_MPEG
-        long rate;
-        int channels,encoding;
-
-        mpeg_data = init_mpeg_codec_data(streamFile, start_offset, -1, -1, &mpeg_coding_type); // -1 to not check sample rate or channels
-        if (!mpeg_data) goto fail;
-
-        if (MPG123_OK != mpg123_getformat(mpeg_data->m,&rate,&channels,&encoding)) goto fail;
-        channel_count = channels;
-        sample_rate = rate;
-
-#else
-        // reject if no MPEG support
-        goto fail;
-#endif
-
-  }
-
-			loop_flag=read_32bitBE(0x18,streamFile);
-
-
-   /* build the VGMSTREAM */
+    channel_count = read_32bitBE(0x8,streamFile);
+    codec_id = read_32bitBE(0x4,streamFile);
+    
+    /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-    vgmstream->sample_rate = sample_rate;
-    vgmstream->num_samples = num_samples;
-    vgmstream->channels = channel_count;
-    vgmstream->meta_type = meta_PS3_MSF;
-	
-	if(mp3ID==0xFFFA) {
+    /* fill in the vital statistics */
+	  vgmstream->channels = channel_count;
+    vgmstream->sample_rate = read_32bitBE(0x10,streamFile);
+    start_offset = 0x40;
+
+    switch (codec_id) {
 #ifdef VGM_USE_MPEG
-        /* NOTE: num_samples seems to be quite wrong for MPEG */
-        vgmstream->codec_data = mpeg_data;
-		vgmstream->layout_type = layout_mpeg;
-		vgmstream->coding_type = mpeg_coding_type;
-#else
-        // reject if no MPEG support
-        goto fail;
+        case 0x7:
+            /* MPEG */
+            {
+                mpeg_codec_data *mpeg_data = NULL;
+                struct mpg123_frameinfo mi;
+                coding_t ct;
+
+                mpeg_data = init_mpeg_codec_data(streamFile, start_offset, vgmstream->sample_rate, vgmstream->channels, &ct);
+                if (!mpeg_data) goto fail;
+                vgmstream->codec_data = mpeg_data;
+
+                if (MPG123_OK != mpg123_info(mpeg_data->m, &mi)) goto fail;
+
+                vgmstream->coding_type = ct;
+                vgmstream->layout_type = layout_mpeg;
+                if (mi.vbr != MPG123_CBR) goto fail;
+                vgmstream->num_samples = mpeg_bytes_to_samples(read_32bitBE(0xC,streamFile), &mi);
+                vgmstream->num_samples -= vgmstream->num_samples%576;
+                if (loop_flag) {
+                    vgmstream->loop_start_sample = mpeg_bytes_to_samples(loop_start, &mi);
+                    vgmstream->loop_start_sample -= vgmstream->loop_start_sample%576;
+                    vgmstream->loop_end_sample = mpeg_bytes_to_samples(loop_end, &mi);
+                    vgmstream->loop_end_sample -= vgmstream->loop_end_sample%576;
+                }
+                vgmstream->interleave_block_size = 0;
+            }
+            break;
 #endif
-	}
+        default:
+            goto fail;
+    }
 
-
-	if (loop_flag) {
-			vgmstream->loop_start_sample = read_32bitBE(0x18,streamFile)/960*1152;
-			vgmstream->loop_end_sample = read_32bitBE(0x1C,streamFile)/960*1152;
-  }
-
+    vgmstream->meta_type = meta_PS3_MSF;
 
     /* open the file for reading */
     {
         int i;
         STREAMFILE * file;
-		if(vgmstream->layout_type == layout_interleave) {
-			file = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
-			if (!file) goto fail;
-			for (i=0;i<channel_count;i++) {
-				vgmstream->ch[i].streamfile = file;
+        file = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
+        if (!file) goto fail;
+        for (i=0;i<channel_count;i++) {
+            vgmstream->ch[i].streamfile = file;
 
-				vgmstream->ch[i].channel_start_offset=
-					vgmstream->ch[i].offset=start_offset+
-					vgmstream->interleave_block_size*i;
-
-			}
-		}
-#ifdef VGM_USE_MPEG
-		else if(vgmstream->layout_type == layout_mpeg) {
-			for (i=0;i<channel_count;i++) {
-				vgmstream->ch[i].streamfile = streamFile->open(streamFile,filename,MPEG_BUFFER_SIZE);
-				vgmstream->ch[i].channel_start_offset= vgmstream->ch[i].offset=start_offset;
-			}
+            vgmstream->ch[i].channel_start_offset=
+                vgmstream->ch[i].offset=start_offset;
 
         }
-#endif
-        else { goto fail; }
     }
 
     return vgmstream;
 
     /* clean up anything we may have opened */
 fail:
-#ifdef VGM_USE_MPEG
-    if (mpeg_data) {
-        mpg123_delete(mpeg_data->m);
-        free(mpeg_data);
-
-        if (vgmstream) {
-            vgmstream->codec_data = NULL;
-        }
-    }
-#endif
     if (vgmstream) close_vgmstream(vgmstream);
     return NULL;
 }
