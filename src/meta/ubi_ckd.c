1b41d008e6a0ab8ebd96002e105d6655c92f133b
Chris Moeller <kode54@gmail.com>
2013-09-03 11:20:45 -0700

    Fixed path limit for new Ubisoft CKD handler

--- src/meta/ubi_ckd.c
+++ src/meta/ubi_ckd.c
@@ -6,7 +6,7 @@
 /* Ubisoft CKD (Rayman Origins - Wii) */
 VGMSTREAM * init_vgmstream_ubi_ckd(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
-    char filename[260];
+    char filename[PATH_LIMIT];
     off_t start_offset;
 
 	int loop_flag;
