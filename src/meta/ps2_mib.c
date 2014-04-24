bb59d6a3894f76e1e3866d7610126b5269531f35
Adam Gashlin <halleyscometsw@users.sourceforge.net>
2014-04-23 21:25:30 -0700

    remove nonsense (thanks kode)

--- src/meta/ps2_mib.c
+++ src/meta/ps2_mib.c
@@ -274,8 +274,6 @@ VGMSTREAM * init_vgmstream_ps2_mib(STREAMFILE *streamFile) {
 			vgmstream->interleave_block_size=0;
 			vgmstream->sample_rate = 22050;
 			vgmstream->channels = 1;
-			vgmstream->start_ch = 1;
-			vgmstream->loop_ch = 1;
 		}
 
 		vgmstream->num_samples = (int32_t)(fileLength/16/channel_count*28);
