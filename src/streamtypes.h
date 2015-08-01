6a9dadc5442309e10b532cf93b671124ee1f992f
Chris Moeller <kode54@gmail.com>
2015-08-01 03:28:53 +0000

    Updated for Visual Studio 2015

--- src/streamtypes.h
+++ src/streamtypes.h
@@ -7,10 +7,14 @@
 #define _STREAMTYPES_H
 
 #ifdef _MSC_VER
+#if _MSC_VER < 1400
 #include <pstdint.h>
+#define snprintf _snprintf
+#else
+#include <stdint.h>
+#endif
 #define inline _inline
 #define strcasecmp _stricmp
-#define snprintf _snprintf
 #else
 #include <stdint.h>
 #endif
