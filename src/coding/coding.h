81d1f410a16dd0cdb923fb4e9ebb21e43f06366d
bnnm <bananaman255@gmail.com>
2016-11-15 23:45:02 +0100

    Fix missing decode_hca

--- src/coding/coding.h
+++ src/coding/coding.h
@@ -135,4 +135,6 @@ void decode_lsf(VGMSTREAMCHANNEL * stream, sample * outbuf, int channelspacing, 
 
 void decode_mtaf(VGMSTREAMCHANNEL * stream, sample * outbuf, int channelspacing, int32_t first_sample, int32_t samples_to_do, int channel, int channels);
 
+void decode_hca(hca_codec_data * data, sample * outbuf, int32_t samples_to_do, int channels);
+
 #endif
