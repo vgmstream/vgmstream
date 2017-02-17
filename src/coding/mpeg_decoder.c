2e41586e6861f3e79fa10b8ef0fd365cd5e664de
bnnm <bananaman255@gmail.com>
2017-02-17 18:35:58 +0100

    Move AHX MPEG init code to mpeg_decoder and other cleanup

--- src/coding/mpeg_decoder.c
+++ src/coding/mpeg_decoder.c
@@ -6,84 +6,6 @@
 #include "coding.h"
 #include "../util.h"
 
-/* mono, mpg123 expects frames of 0x414 (160kbps, 22050Hz) but they
- * actually vary and are much shorter */
-void decode_fake_mpeg2_l2(VGMSTREAMCHANNEL *stream,
-        mpeg_codec_data * data,
-        sample * outbuf, int32_t samples_to_do) {
-    int samples_done = 0;
-
-    while (samples_done < samples_to_do) {
-        size_t bytes_done;
-        int rc;
-
-        if (!data->buffer_full) {
-            /* fill buffer up to next frame ending (or file ending) */
-            int bytes_into_header = 0;
-            const uint8_t header[4] = {0xff,0xf5,0xe0,0xc0};
-            off_t frame_offset = 0;
-
-            /* assume that we are starting at a header, skip it and look for the
-             * next one */
-            read_streamfile(data->buffer, stream->offset+frame_offset, 4,
-                    stream->streamfile);
-            frame_offset += 4;
-
-            do {
-                uint8_t byte;
-                byte =
-                    read_8bit(stream->offset+frame_offset,stream->streamfile);
-                data->buffer[frame_offset] = byte;
-                frame_offset++;
-
-                if (byte == header[bytes_into_header]) {
-                    bytes_into_header++;
-                } else {
-                    /* This might have been the first byte of the header, so
-                     * we need to check again.
-                     * No need to get more complicated than this, though, since
-                     * there are no repeated characters in the search string. */
-                    if (bytes_into_header>0) {
-                        frame_offset--;
-                    }
-                    bytes_into_header=0;
-                }
-
-                if (bytes_into_header==4) {
-                    break;
-                }
-            } while (frame_offset < AHX_EXPECTED_FRAME_SIZE);
-
-            if (bytes_into_header==4) frame_offset-=4;
-            memset(data->buffer+frame_offset,0,
-                    AHX_EXPECTED_FRAME_SIZE-frame_offset);
-
-            data->buffer_full = 1;
-            data->buffer_used = 0;
-
-            stream->offset += frame_offset;
-        }
-
-        if (!data->buffer_used) {
-            rc = mpg123_decode(data->m,
-                    data->buffer,AHX_EXPECTED_FRAME_SIZE,
-                    (unsigned char *)(outbuf+samples_done),
-                    (samples_to_do-samples_done)*sizeof(sample),
-                    &bytes_done);
-            data->buffer_used = 1;
-        } else {
-            rc = mpg123_decode(data->m,
-                    NULL,0,
-                    (unsigned char *)(outbuf+samples_done),
-                    (samples_to_do-samples_done)*sizeof(sample),
-                    &bytes_done);
-        }
-
-        if (rc == MPG123_NEED_MORE) data->buffer_full = 0;
-
-        samples_done += bytes_done/sizeof(sample);
-    }
-}
 
 mpeg_codec_data *init_mpeg_codec_data(STREAMFILE *streamfile, off_t start_offset, long given_sample_rate, int given_channels, coding_t *coding_type, int * actual_sample_rate, int * actual_channels) {
     int rc;
@@ -168,13 +90,38 @@ mpeg_codec_data *init_mpeg_codec_data(STREAMFILE *streamfile, off_t start_offset
     return data;
 
 mpeg_fail:
-    fprintf(stderr, "mpeg_fail start_offset=%x\n",(unsigned int)start_offset);
-    if (data) {
-        mpg123_delete(data->m);
-        free(data);
-    }
+    free_mpeg(data);
     return NULL;
 }
+
+mpeg_codec_data *init_mpeg_codec_data_ahx(STREAMFILE *streamFile, off_t start_offset, int channel_count) {
+    mpeg_codec_data *data = NULL;
+    int rc;
+
+    data = calloc(1,sizeof(mpeg_codec_data));
+    if (!data) goto fail;
+
+    data->m = mpg123_new(NULL,&rc);
+    if (rc == MPG123_NOT_INITIALIZED) {
+        if (mpg123_init() != MPG123_OK) goto fail;
+
+        data->m = mpg123_new(NULL,&rc);
+        if (rc != MPG123_OK) goto fail;
+    } else if (rc!=MPG123_OK) {
+        goto fail;
+    }
+
+    if (mpg123_open_feed(data->m) != MPG123_OK) {
+        goto fail;
+    }
+
+    return data;
+
+fail:
+    free_mpeg(data);
+    return NULL;
+}
+
 
 /* decode anything mpg123 can */
 void decode_mpeg(VGMSTREAMCHANNEL *stream,
@@ -219,6 +166,85 @@ void decode_mpeg(VGMSTREAMCHANNEL *stream,
         if (rc == MPG123_NEED_MORE) data->buffer_full = 0;
 
         samples_done += bytes_done/sizeof(sample)/channels;
+    }
+}
+
+/* mono, mpg123 expects frames of 0x414 (160kbps, 22050Hz) but they
+ * actually vary and are much shorter */
+void decode_fake_mpeg2_l2(VGMSTREAMCHANNEL *stream,
+        mpeg_codec_data * data,
+        sample * outbuf, int32_t samples_to_do) {
+    int samples_done = 0;
+
+    while (samples_done < samples_to_do) {
+        size_t bytes_done;
+        int rc;
+
+        if (!data->buffer_full) {
+            /* fill buffer up to next frame ending (or file ending) */
+            int bytes_into_header = 0;
+            const uint8_t header[4] = {0xff,0xf5,0xe0,0xc0};
+            off_t frame_offset = 0;
+
+            /* assume that we are starting at a header, skip it and look for the
+             * next one */
+            read_streamfile(data->buffer, stream->offset+frame_offset, 4,
+                    stream->streamfile);
+            frame_offset += 4;
+
+            do {
+                uint8_t byte;
+                byte =
+                    read_8bit(stream->offset+frame_offset,stream->streamfile);
+                data->buffer[frame_offset] = byte;
+                frame_offset++;
+
+                if (byte == header[bytes_into_header]) {
+                    bytes_into_header++;
+                } else {
+                    /* This might have been the first byte of the header, so
+                     * we need to check again.
+                     * No need to get more complicated than this, though, since
+                     * there are no repeated characters in the search string. */
+                    if (bytes_into_header>0) {
+                        frame_offset--;
+                    }
+                    bytes_into_header=0;
+                }
+
+                if (bytes_into_header==4) {
+                    break;
+                }
+            } while (frame_offset < AHX_EXPECTED_FRAME_SIZE);
+
+            if (bytes_into_header==4) frame_offset-=4;
+            memset(data->buffer+frame_offset,0,
+                    AHX_EXPECTED_FRAME_SIZE-frame_offset);
+
+            data->buffer_full = 1;
+            data->buffer_used = 0;
+
+            stream->offset += frame_offset;
+        }
+
+        if (!data->buffer_used) {
+            rc = mpg123_decode(data->m,
+                    data->buffer,AHX_EXPECTED_FRAME_SIZE,
+                    (unsigned char *)(outbuf+samples_done),
+                    (samples_to_do-samples_done)*sizeof(sample),
+                    &bytes_done);
+            data->buffer_used = 1;
+        } else {
+            rc = mpg123_decode(data->m,
+                    NULL,0,
+                    (unsigned char *)(outbuf+samples_done),
+                    (samples_to_do-samples_done)*sizeof(sample),
+                    &bytes_done);
+        }
+
+        if (rc == MPG123_NEED_MORE) data->buffer_full = 0;
+
+        samples_done += bytes_done/sizeof(sample);
     }
 }
 
