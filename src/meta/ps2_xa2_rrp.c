d0ecf3745870b9de85060858655e02f6c645d3a1
bnnm <bananaman255@gmail.com>
2017-12-06 18:16:03 +0100

    Move .XA2 RRP to its own file for consistency/clarity

--- src/meta/ps2_xa2_rrp.c
+++ src/meta/ps2_xa2_rrp.c
@@ -1,40 +1,39 @@
 #include "meta.h"
-#include "../util.h"
 
-/* XA2 (XG3 Extreme-G Racing) */
-VGMSTREAM * init_vgmstream_ps2_xa2(STREAMFILE *streamFile) {
+
+/* XA2 (RC Revenge Pro) */
+VGMSTREAM * init_vgmstream_ps2_xa2_rrp(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
     char filename[PATH_LIMIT];
     off_t start_offset;
+
     int loop_flag = 0;
-	int channel_count;
+    int channel_count;
 
     /* check extension, case insensitive */
     streamFile->get_name(streamFile,filename,sizeof(filename));
     if (strcasecmp("xa2",filename_extension(filename))) goto fail;
 
+    /* check header */
+    if (read_32bitBE(0xC,streamFile) != 0x00000000)
+        goto fail;
+
     loop_flag = 0;
     channel_count = read_32bitLE(0x0,streamFile);
 
-    if (read_32bitLE(0x4,streamFile) > 0x1000) goto fail;
-
-	/* build the VGMSTREAM */
+    /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-	/* fill in the vital statistics */
+    /* fill in the vital statistics */
     start_offset = 0x800;
-	vgmstream->channels = channel_count;
+    vgmstream->channels = channel_count;
     vgmstream->sample_rate = 44100;
     vgmstream->coding_type = coding_PSX;
-    vgmstream->num_samples = (get_streamfile_size(streamFile)-start_offset)/channel_count/16*28;
-    if (loop_flag) {
-        vgmstream->loop_start_sample = 0;
-        vgmstream->loop_end_sample = vgmstream->num_samples;
-    }
+    vgmstream->num_samples = (get_streamfile_size(streamFile)-0x800)*28/16/channel_count;
     vgmstream->layout_type = layout_interleave;
-    vgmstream->interleave_block_size = read_32bitLE(0x04,streamFile);
-    vgmstream->meta_type = meta_PS2_XA2;
+    vgmstream->interleave_block_size = 0x1000;
+    vgmstream->meta_type = meta_PS2_XA2_RRP;
 
     /* open the file for reading */
     {
