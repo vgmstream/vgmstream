9928380c4911ea76eae232fafa7b16a5cb1e2c40
Christopher Snowhill <kode54@gmail.com>
2018-01-04 18:59:29 -0800

    Add libatrac9 for Windows builds.

---
+++ ext_includes/libatrac9.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef COMPILING_DLL 
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllimport)  
+#endif
+
+#define ATRAC9_CONFIG_DATA_SIZE 4
+
+typedef struct {
+	int channels;
+	int channelConfigIndex;
+	int samplingRate;
+	int superframeSize;
+	int framesInSuperframe;
+	int frameSamples;
+	int wlength;
+	unsigned char configData[ATRAC9_CONFIG_DATA_SIZE];
+} Atrac9CodecInfo;
+
+DLLEXPORT void* Atrac9GetHandle(void);
+DLLEXPORT void Atrac9ReleaseHandle(void* handle);
+
+DLLEXPORT int Atrac9InitDecoder(void* handle, unsigned char *pConfigData);
+DLLEXPORT int Atrac9Decode(void* handle, const unsigned char *pAtrac9Buffer, short *pPcmBuffer, int *pNBytesUsed);
+
+DLLEXPORT int Atrac9GetCodecInfo(void* handle, Atrac9CodecInfo *pCodecInfo);
+
+#ifdef __cplusplus
+}
+#endif
