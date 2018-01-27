561c3fa8c17624b8ecbc31835062d4b33b80c35b
bnnm <bananaman255@gmail.com>
2018-01-27 12:21:25 +0100

    Add Nippon Ichi SPS subfiles [ClaDun (PSP)]

--- src/meta/meta.h
+++ src/meta/meta.h
@@ -700,4 +700,6 @@ VGMSTREAM * init_vgmstream_fsb_encrypted(STREAMFILE * streamFile);
 VGMSTREAM * init_vgmstream_xwc(STREAMFILE *streamFile);
 
 VGMSTREAM * init_vgmstream_atsl3(STREAMFILE *streamFile);
+
+VGMSTREAM * init_vgmstream_sps_n1(STREAMFILE *streamFile);
 #endif /*_META_H*/
