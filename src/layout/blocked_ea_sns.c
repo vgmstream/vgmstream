c3b637a199a4e5f563dc241d636e83147db2130d
bnnm <bananaman255@gmail.com>
2018-02-03 17:19:38 +0100

    Add EA SNS/SPS DSP [Need for Speed Nitro sfx (Wii)]

--- src/layout/blocked_ea_sns.c
+++ src/layout/blocked_ea_sns.c
@@ -7,6 +7,8 @@ void block_update_ea_sns(off_t block_offset, VGMSTREAM * vgmstream) {
     STREAMFILE* streamFile = vgmstream->ch[0].streamfile;
     uint32_t block_size, block_samples;
     size_t file_size = get_streamfile_size(streamFile);
+    off_t channel_start;
+    size_t channel_interleave;
     int i;
 
     /* always BE */
@@ -26,9 +28,26 @@ void block_update_ea_sns(off_t block_offset, VGMSTREAM * vgmstream) {
      * - in SPS: 0x48=header, 0x44=normal block, 0x45=last block (empty) */
     block_size &= 0x00FFFFFF;
 
+    switch(vgmstream->coding_type) {
+        case coding_NGC_DSP:
+            /* 0x04: unknown (0x00/02), 0x08: some size?, 0x34: null? */
+            channel_start      = read_32bitBE(block_offset+0x08+0x00,streamFile);
+            channel_interleave = read_32bitBE(block_offset+0x08+0x0c,streamFile);
+            /* guessed as all known EA DSP only have one block with subheader (maybe changes coefs every block?) */
+            if (channel_start >= 0x40) {
+                dsp_read_coefs_be(vgmstream,streamFile, block_offset+0x08+0x10,0x28);
+                dsp_read_hist_be (vgmstream,streamFile, block_offset+0x08+0x30,0x28);//todo guessed and doesn't fix clicks in full loops
+            }
+            break;
+
+        default:
+            channel_start = 0x00;
+            channel_interleave = 0x00;
+            break;
+    }
+
     for (i = 0; i < vgmstream->channels; i++) {
-        off_t channel_start = 0x00;
-        vgmstream->ch[i].offset = block_offset + 0x08 + channel_start;
+        vgmstream->ch[i].offset = block_offset + 0x08 + channel_start + i*channel_interleave;
 
         /* also fix first offset (for EALayer3) */
         if (block_offset == vgmstream->ch[i].channel_start_offset) {
