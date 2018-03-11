9062541aa2d25c6778ba78808d82426984e396e1
bnnm <bananaman255@gmail.com>
2018-03-11 20:30:32 +0100

    Fix GCA being off by one sample in some cases, cleanup [BlowOut (GC)]

--- src/meta/gca.c
+++ src/meta/gca.c
@@ -1,69 +1,42 @@
 #include "meta.h"
+#include "../coding/coding.h"
 #include "../util.h"
 
-/* GCA (from Metal Slug Anthology [Wii]) */
+/* GCA - Terminal Reality games [Metal Slug Anthology (Wii), BlowOut (GC)] */
 VGMSTREAM * init_vgmstream_gca(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
-    char filename[PATH_LIMIT];
     off_t start_offset;
-    int loop_flag = 0;
-	int channel_count = 1;
+    int loop_flag, channel_count;
 
-    /* check extension, case insensitive */
-    streamFile->get_name(streamFile,filename,sizeof(filename));
-    if (strcasecmp("gca",filename_extension(filename))) goto fail;
+    /* checks */
+    if (!check_extensions(streamFile, "gca"))
+        goto fail;
 
-    /* check header */
     if (read_32bitBE(0x00,streamFile) != 0x47434131) /* "GCA1" */
         goto fail;
-    
+
+    start_offset = 0x40;
+    loop_flag = 0;
+    channel_count = 1;
+
 	/* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-	/* fill in the vital statistics */
-    start_offset = 0x40;
-	vgmstream->channels = channel_count;
     vgmstream->sample_rate = read_32bitBE(0x2A,streamFile);
     vgmstream->coding_type = coding_NGC_DSP;
-    vgmstream->num_samples = read_32bitBE(0x26,streamFile)*7/8;
-    if (loop_flag) {
-        vgmstream->loop_start_sample = 0;
-        vgmstream->loop_end_sample = read_32bitBE(0x26,streamFile)*7/8;
-    }
+    vgmstream->num_samples = dsp_nibbles_to_samples(read_32bitBE(0x26,streamFile));//read_32bitBE(0x26,streamFile)*7/8;
 
-	/* We have no interleave, so we have no layout */
-    vgmstream->layout_type = layout_none;
+    vgmstream->layout_type = layout_none; /* we have no interleave, so we have no layout */
     vgmstream->meta_type = meta_GCA;
 
-    /* open the file for reading */
-    {
-        int i;
-        STREAMFILE * file;
-        file = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
-        if (!file) goto fail;
-        for (i=0;i<channel_count;i++) {
-            vgmstream->ch[i].streamfile = file;
+    dsp_read_coefs_be(vgmstream,streamFile,0x04,0x00);
 
-			vgmstream->ch[i].channel_start_offset=
-                vgmstream->ch[i].offset=start_offset+
-                vgmstream->interleave_block_size*i;
-
-        }
-    }
-    
-	/*Retrieving the coef table */
-	{
-        int i;
-        for (i=0;i<16;i++) {
-            vgmstream->ch[0].adpcm_coef[i] = read_16bitBE(0x04+i*2,streamFile);
-        }
-    }
-
+    if (!vgmstream_open_stream(vgmstream,streamFile,start_offset))
+        goto fail;
     return vgmstream;
 
-    /* clean up anything we may have opened */
 fail:
-    if (vgmstream) close_vgmstream(vgmstream);
+    close_vgmstream(vgmstream);
     return NULL;
 }
