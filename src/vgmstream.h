2df613d8932edb10269647d07e50afd768c17f09
bnnm <bananaman255@gmail.com>
2018-07-06 21:04:06 +0200

    Fix various music .ivaud [GTA IV (PC)]

--- src/vgmstream.h
+++ src/vgmstream.h
@@ -503,7 +503,7 @@ typedef enum {
     meta_PS2_MCG,           /* Gunvari MCG Files (was name .GCM on disk) */
     meta_ZSD,               /* Dragon Booster ZSD */
     meta_RedSpark,          /* "RedSpark" RSD (MadWorld) */
-    meta_PC_IVAUD,          /* .ivaud GTA IV */
+    meta_IVAUD,          /* .ivaud GTA IV */
     meta_NDS_HWAS,          /* Spider-Man 3, Tony Hawk's Downhill Jam, possibly more... */
     meta_NGC_LPS,           /* Rave Master (Groove Adventure Rave)(GC) */
     meta_NAOMI_ADPCM,       /* NAOMI/NAOMI2 ARcade games */
