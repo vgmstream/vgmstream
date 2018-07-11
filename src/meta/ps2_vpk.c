f5551f741596fd2fa50bea4593f43f44821181b2
Christopher Snowhill <kode54@gmail.com>
2018-07-10 17:23:04 -0700

    Update ps2_vpk.c
    Fix channels variable references.

--- src/meta/ps2_vpk.c
+++ src/meta/ps2_vpk.c
@@ -34,7 +34,7 @@ VGMSTREAM * init_vgmstream_ps2_vpk(STREAMFILE *streamFile) {
 
     /* Check for Compression Scheme */
     vgmstream->coding_type = coding_PSX;
-    vgmstream->num_samples = ps_bytes_to_samples(read_32bitLE(0x04,streamFile),channels);
+    vgmstream->num_samples = ps_bytes_to_samples(read_32bitLE(0x04,streamFile),vgmstream->channels);
 
     /* Get loop point values */
     if(vgmstream->loop_flag) {
@@ -42,7 +42,7 @@ VGMSTREAM * init_vgmstream_ps2_vpk(STREAMFILE *streamFile) {
         vgmstream->loop_end_sample = vgmstream->num_samples;
     }
 
-    vgmstream->interleave_block_size = read_32bitLE(0x0C,streamFile)/channels;
+    vgmstream->interleave_block_size = read_32bitLE(0x0C,streamFile)/vgmstream->channels;
     vgmstream->layout_type = layout_interleave;
     vgmstream->meta_type = meta_PS2_VPK;
 
