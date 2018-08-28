f7c26a493d34c8814697b655e750007fcb05d3ae
bnnm <bananaman255@gmail.com>
2018-08-28 20:35:07 +0200

    Clean IDSPs

--- src/meta/idsp.c
+++ src/meta/idsp.c
@@ -1,171 +1,97 @@
 #include "meta.h"
+#include "../coding/coding.h"
 
-//todo cleanup
 
 /* "idsp" - from Namco's Wii NUB archives [Soul Calibur Legends (Wii), Sky Crawlers: Innocent Aces (Wii)] */
 VGMSTREAM * init_vgmstream_nub_idsp(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
-    char filename[PATH_LIMIT];
-    int loop_flag;
-    int channel_count;
-    int i, j;
     off_t start_offset;
+    int loop_flag, channel_count;
 
-    /* check extension, case insensitive */
-    streamFile->get_name(streamFile,filename,sizeof(filename));
-    if (strcasecmp("idsp",filename_extension(filename))) goto fail;
+    /* checks */
+    if ( !check_extensions(streamFile,"idsp") )
+        goto fail;
 
-    /* check header */
-    if (read_32bitBE(0x00,streamFile) != 0x69647370 ||  /* "idsp" */
-        read_32bitBE(0xBC,streamFile) != 0x49445350)    /* "IDSP" */
+    /* actual header starts at "IDSP", while "idsp" is mostly nub bank stuff */
+    if (read_32bitBE(0x00,streamFile) != 0x69647370)    /* "idsp" */
+        goto fail;
+    if (read_32bitBE(0xBC,streamFile) != 0x49445350)    /* "IDSP" */
         goto fail;
 
     loop_flag = read_32bitBE(0x20,streamFile);
     channel_count = read_32bitBE(0xC4,streamFile);
-    
-    if (channel_count > 8)
-        goto fail;
+    if (channel_count > 8) goto fail;
+
+    start_offset = 0x100 + (channel_count * 0x60);
 
 
     /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-    start_offset = (channel_count * 0x60) + 0x100;
+    vgmstream->meta_type = meta_NUB_IDSP;
     vgmstream->sample_rate = read_32bitBE(0xC8,streamFile);
-    vgmstream->coding_type = coding_NGC_DSP;
-    vgmstream->num_samples = (read_32bitBE(0x14,streamFile))*14/8/channel_count;
+    vgmstream->num_samples = dsp_bytes_to_samples(read_32bitBE(0x14,streamFile),channel_count);
     if (loop_flag) {
         vgmstream->loop_start_sample = (read_32bitBE(0xD0,streamFile));
         vgmstream->loop_end_sample = (read_32bitBE(0xD4,streamFile));
     }
 
-    if (channel_count == 1) {
-        vgmstream->layout_type = layout_none;
-    }
-    else if (channel_count > 1) {
-        if (read_32bitBE(0xD8,streamFile) == 0) {
-              vgmstream->layout_type = layout_none;
-              vgmstream->interleave_block_size = (get_streamfile_size(streamFile)-start_offset)/2;
-        }
-        else if (read_32bitBE(0xD8,streamFile) > 0) {
-              vgmstream->layout_type = layout_interleave;
-              vgmstream->interleave_block_size = read_32bitBE(0xD8,streamFile);
-        }
-    }
+    vgmstream->coding_type = coding_NGC_DSP;
+    vgmstream->layout_type = layout_interleave;
+    vgmstream->interleave_block_size = read_32bitBE(0xD8,streamFile);
+    if (vgmstream->interleave_block_size == 0)
+        vgmstream->interleave_block_size = (get_streamfile_size(streamFile) - start_offset) / channel_count;
 
-    vgmstream->meta_type = meta_NUB_IDSP;
+    dsp_read_coefs_be(vgmstream,streamFile,0x118,0x60);
 
-    {
-        if (vgmstream->coding_type == coding_NGC_DSP) {
-            off_t coef_table[8] = {0x118,0x178,0x1D8,0x238,0x298,0x2F8,0x358,0x3B8};
-            for (j=0;j<vgmstream->channels;j++) {
-                for (i=0;i<16;i++) {
-                    vgmstream->ch[j].adpcm_coef[i] = read_16bitBE(coef_table[j]+i*2,streamFile);
-                }
-            }
-        }
-    }
-
-    /* open the file for reading */
-    {
-        int i;
-        STREAMFILE * file;
-        file = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
-        if (!file) goto fail;
-        for (i=0;i<channel_count;i++) {
-            vgmstream->ch[i].streamfile = file;
-
-            vgmstream->ch[i].channel_start_offset=
-                vgmstream->ch[i].offset=start_offset+
-                vgmstream->interleave_block_size*i;
-
-        }
-    }
-
-
+    if ( !vgmstream_open_stream(vgmstream, streamFile, start_offset) )
+        goto fail;
     return vgmstream;
 
-    /* clean up anything we may have opened */
 fail:
-    if (vgmstream) close_vgmstream(vgmstream);
+    close_vgmstream(vgmstream);
     return NULL;
 }
+
 
 /* IDSP - from Inevitable Entertainment games [Defender (GC)] */
 VGMSTREAM * init_vgmstream_idsp_ie(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
-    char filename[PATH_LIMIT];
-    int loop_flag = 0;
-    int channel_count;
     off_t start_offset;
+    int loop_flag, channel_count;
 
-    /* check extension, case insensitive */
-    streamFile->get_name(streamFile,filename,sizeof(filename));
-    if (strcasecmp("idsp",filename_extension(filename))) goto fail;
 
-    /* check header */
+    /* checks */
+    if ( !check_extensions(streamFile,"idsp") )
+        goto fail;
     if (read_32bitBE(0x00,streamFile) != 0x49445350) /* "IDSP" */
         goto fail;
 
+    loop_flag = 0;
     channel_count = read_32bitBE(0x0C,streamFile);
-    
-    if (channel_count > 2) // Refuse everything else for now
-        goto fail;
+    if (channel_count > 2) goto fail;
+    start_offset = 0x70;
+
 
     /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count,loop_flag);
     if (!vgmstream) goto fail;
 
-    start_offset = 0x70;
+    vgmstream->meta_type = meta_IDSP_IE;
     vgmstream->sample_rate = read_32bitBE(0x08,streamFile);
     vgmstream->coding_type = coding_NGC_DSP;
-    vgmstream->num_samples = read_32bitBE(0x04,streamFile)/channel_count/8*14;
-    if (loop_flag) {
-        vgmstream->loop_start_sample = 0;
-        vgmstream->loop_end_sample = read_32bitBE(0x04,streamFile)/channel_count/8*14;
-    }
+    vgmstream->num_samples = dsp_bytes_to_samples(read_32bitBE(0x04,streamFile), channel_count);
 
-    if (channel_count == 1) {
-        vgmstream->layout_type = layout_none;
-    }
-    else {
-        vgmstream->layout_type = layout_interleave;
-        vgmstream->interleave_block_size = read_32bitBE(0x10,streamFile);
-    }
-    
-    vgmstream->meta_type = meta_IDSP_IE;
+    vgmstream->layout_type = layout_interleave;
+    vgmstream->interleave_block_size = read_32bitBE(0x10,streamFile);
+    dsp_read_coefs_be(vgmstream,streamFile,0x14,0x2E);
 
-    {
-        int i;
-        for (i=0;i<16;i++)
-            vgmstream->ch[0].adpcm_coef[i] = read_16bitBE(0x14+i*2,streamFile);
-        if (channel_count == 2) {
-            for (i=0;i<16;i++)
-                vgmstream->ch[1].adpcm_coef[i] = read_16bitBE(0x42+i*2,streamFile);
-        }
-    }
-
-    /* open the file for reading */
-    {
-        int i;
-        STREAMFILE * file;
-        file = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
-        if (!file) goto fail;
-        for (i=0;i<channel_count;i++) {
-            vgmstream->ch[i].streamfile = file;
-
-            vgmstream->ch[i].channel_start_offset=
-                vgmstream->ch[i].offset=start_offset+
-                vgmstream->interleave_block_size*i;
-
-        }
-    }
-
+    if ( !vgmstream_open_stream(vgmstream, streamFile, start_offset) )
+        goto fail;
     return vgmstream;
 
-    /* clean up anything we may have opened */
 fail:
-    if (vgmstream) close_vgmstream(vgmstream);
+    close_vgmstream(vgmstream);
     return NULL;
 }
