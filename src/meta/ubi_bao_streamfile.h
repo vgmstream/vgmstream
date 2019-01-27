fc8911b68ab42d7ba32102425efaca3941f49c07
bnnm <bananaman255@gmail.com>
2019-01-27 23:42:42 +0100

    Prepare BAO layers/sequences

---
+++ src/meta/ubi_bao_streamfile.h
@@ -0,0 +1,11 @@
+#ifndef _UBI_BAO_STREAMFILE_H_
+#define _UBI_BAO_STREAMFILE_H_
+
+//todo fix dupe code, but would be nice to keep it all in separate compilation units
+#include "ubi_sb_streamfile.h"
+
+static STREAMFILE* setup_ubi_bao_streamfile(STREAMFILE *streamFile, off_t stream_offset, size_t stream_size, int layer_number, int layer_count, int big_endian) {
+    return setup_ubi_sb_streamfile(streamFile, stream_offset, stream_size, layer_number, layer_count, big_endian);
+}
+
+#endif /* _UBI_BAO_STREAMFILE_H_ */
