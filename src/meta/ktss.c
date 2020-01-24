3ae88b53454a71c88d63e16349a925eb5760de59
bnnm <bananaman255@gmail.com>
2020-01-24 20:47:03 +0100

    Fix .ktss with VBR Opus [Warriors Orochi 4 Ultimate DLC (Switch)]

--- src/meta/ktss.c
+++ src/meta/ktss.c
@@ -3,52 +3,59 @@
 
 VGMSTREAM * init_vgmstream_ktss(STREAMFILE *streamFile) {
     VGMSTREAM * vgmstream = NULL;
-    int loop_flag, channel_count;
+    int loop_flag, channel_count, sample_rate;
     int8_t version, num_layers, codec_id;
-    int32_t loop_length, coef_start_offset, coef_spacing;
+    int32_t num_samples, loop_start, loop_length, coef_offset, coef_spacing;
     off_t start_offset;
     size_t data_size, skip = 0;
 
     if (!check_extensions(streamFile, "kns,ktss"))
         goto fail;
 
-    if (read_32bitBE(0, streamFile) != 0x4B545353) /* "KTSS" */
+    if (read_32bitBE(0x00, streamFile) != 0x4B545353) /* "KTSS" */
         goto fail;
+    /* 0x04: data size */
 
     codec_id = read_8bit(0x20, streamFile);
-    loop_length = read_32bitLE(0x38, streamFile);
-    loop_flag = loop_length > 0;
+    /* 0x01: null, part of codec? */
+    version = read_8bit(0x22, streamFile);
+    /* 0x03: same as version? */
+    start_offset = read_32bitLE(0x24, streamFile) + 0x20;
 
     // A layered stream/track model seems to be used in Hyrule Warriors (Switch).
     // It's also present in other Koei Tecmo KNS but the channel count was always
     // explicitly defined in the 0x29 byte and the number of layers was set to 1.
     // Here, 10 channel files are set up with 2 channels in 5 layers.
     // Super hacky on KT's part and ours to implement but it works.
-    num_layers = read_8bit(0x28, streamFile);
-
+    num_layers    = read_8bit(0x28, streamFile);
     channel_count = read_8bit(0x29, streamFile) * num_layers;
+    sample_rate   = read_32bitLE(0x2c, streamFile);
+
+    num_samples = read_32bitLE(0x30, streamFile);
+    loop_start  = read_32bitLE(0x34, streamFile);
+    loop_length = read_32bitLE(0x38, streamFile);
+    loop_flag = loop_length > 0;
+
 
     /* build the VGMSTREAM */
     vgmstream = allocate_vgmstream(channel_count, loop_flag);
     if (!vgmstream) goto fail;
 
-    vgmstream->sample_rate = read_32bitLE(0x2c, streamFile);
-    vgmstream->num_samples = read_32bitLE(0x30, streamFile);
-    vgmstream->loop_start_sample = read_32bitLE(0x34, streamFile);
-    vgmstream->loop_end_sample = vgmstream->loop_start_sample + loop_length;
+    vgmstream->sample_rate = sample_rate;
+    vgmstream->num_samples = num_samples;
+    vgmstream->loop_start_sample = loop_start;
+    vgmstream->loop_end_sample = loop_start + loop_length;
     vgmstream->meta_type = meta_KTSS;
-    start_offset = read_32bitLE(0x24, streamFile) + 0x20;
 
     switch (codec_id) {
-        case 0x2: /* DSP ADPCM - Hyrule Warriors, Fire Emblem Warriors, and other Koei Tecmo games */
+        case 0x2: /* DSP ADPCM - Hyrule Warriors, Fire Emblem Warriors */
             /* check type details */
-            version = read_8bit(0x22, streamFile);
             if (version == 1) {
-                coef_start_offset = 0x40;
+                coef_offset = 0x40;
                 coef_spacing = 0x2e;
             }
-            else if (version == 3) { // Fire Emblem Warriors (Switch)
-                coef_start_offset = 0x5c;
+            else if (version == 3) { /* Fire Emblem Warriors (Switch) */
+                coef_offset = 0x5c;
                 coef_spacing = 0x60;
             }
             else
@@ -57,18 +64,24 @@ VGMSTREAM * init_vgmstream_ktss(STREAMFILE *streamFile) {
             vgmstream->coding_type = coding_NGC_DSP;
             vgmstream->layout_type = layout_interleave;
             vgmstream->interleave_block_size = 0x8;
-            dsp_read_coefs_le(vgmstream, streamFile, coef_start_offset, coef_spacing);
+            dsp_read_coefs_le(vgmstream, streamFile, coef_offset, coef_spacing);
             break;
 
 #ifdef VGM_USE_FFMPEG
         case 0x9: { /* Opus - Dead or Alive Xtreme 3: Scarlet, Fire Emblem: Three Houses */
             opus_config cfg = {0};
 
+            start_offset = read_32bitLE(0x40, streamFile); /* after seek table, if any */
             data_size = read_32bitLE(0x44, streamFile);
+            /* 0x48: seek table start (0 if no seek table) */
+            /* 0x4c: number of frames */
 
-            cfg.channels = vgmstream->channels;
+            /* 0x50: frame size, or 0 if VBR */
+            /* 0x52: samples per frame */
+            /* 0x54: sample rate */
             cfg.skip = read_32bitLE(0x58, streamFile);
-            cfg.sample_rate = vgmstream->sample_rate; /* also at 0x54 */
+            cfg.sample_rate = vgmstream->sample_rate;
+            cfg.channels = vgmstream->channels;
 
             /* this info seems always included even for stereo streams */
             if (vgmstream->channels <= 8) {
@@ -79,6 +92,10 @@ VGMSTREAM * init_vgmstream_ktss(STREAMFILE *streamFile) {
                     cfg.channel_mapping[i] = read_8bit(0x5c + i,streamFile);
                 }
             }
+
+            /* 0x60: null/reserved */
+            /* 0x70: seek table (0x02 * frames) if VBR */
+            /* later games use VBR frames, hence the seek table [Warriors Orochi 4 Ultimate DLC (Switch)] */
 
             vgmstream->codec_data = init_ffmpeg_switch_opus_config(streamFile, start_offset, data_size, &cfg);
             if (!vgmstream->codec_data) goto fail;
