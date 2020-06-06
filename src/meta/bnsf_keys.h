f835f54b34ec5a4c6f52eacca78c51b96fa5855a
bnnm <bananaman255@gmail.com>
2020-06-06 16:09:39 +0200

    Add BNSF key [The iDOLM@STER: One For All (PS3)]

--- src/meta/bnsf_keys.h
+++ src/meta/bnsf_keys.h
@@ -5,7 +5,11 @@ typedef struct {
     const char* key;
 } bnsfkey_info;
 
-/* Known keys, extracted from games' exe/files */
+/* Known keys, from games' exe (iM@S, near "nus" strings) or files (Tales, config in audio bigfiles).
+ *
+ * In memdumps, first 16 chars of key can be found XORed with "Ua#oK3P94vdxX,ft" after AES 'Td'
+ * mix tables (that end with 8D4697A3 A38D4697 97A38D46 4697A38D), then can be cross referenced
+ * with other strings (max 24 chars) in the memdump. */
 static const bnsfkey_info s14key_list[] = {
 
         /* THE iDOLM@STER 2 (PS3/X360) */
@@ -17,6 +21,9 @@ static const bnsfkey_info s14key_list[] = {
         /* Tales of Berseria (PS3) */
         {"SPSLOC13"},
 
+        /* THE iDOLM@STER: One For All (PS3) */
+        {"86c215d7655eefb5c77ae92c"},
+
 };
 
-#endif/*_BNSF_KEYS_H_*/
+#endif /*_BNSF_KEYS_H_*/
