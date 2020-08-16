18d461bc368f56d256642739527a91eed1a24aac
Simon Aarons <40786398+simontime@users.noreply.github.com>
2020-08-16 15:53:13 +1000

    Remove useless lines

--- src/meta/tgc.c
+++ src/meta/tgc.c
@@ -16,11 +16,7 @@ VGMSTREAM * init_vgmstream_tgc(STREAMFILE *streamFile) {
     vgmstream->sample_rate = 8000;
     vgmstream->num_samples = ((uint16_t)read_16bitBE(1, streamFile) - 3) * 2;
     vgmstream->meta_type   = meta_TGC;
-    vgmstream->layout_type = layout_none;
     vgmstream->coding_type = coding_TGC;
-    
-    vgmstream->ch[0].adpcm_history1_32 = 0;
-    vgmstream->ch[0].adpcm_step_index  = 0;
 
     if (!vgmstream_open_stream(vgmstream, streamFile, 3))
         goto fail;
