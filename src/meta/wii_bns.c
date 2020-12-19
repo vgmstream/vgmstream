2aa5173ed6c0048dfa9e9d7f97dc1adffef8ea44
bnnm <bananaman255@gmail.com>
2020-12-19 15:48:15 +0100

    Cleanup

--- src/meta/wii_bns.c
+++ src/meta/wii_bns.c
@@ -3,72 +3,76 @@
 #include "../util.h"
 
 /* BNS - Wii "Banner Sound" disc jingle */
-VGMSTREAM * init_vgmstream_wii_bns(STREAMFILE *streamFile) {
-    VGMSTREAM * vgmstream = NULL;
-    char filename[PATH_LIMIT];
-    off_t BNS_offset;
-    uint32_t info_offset=0,data_offset=0;
+VGMSTREAM* init_vgmstream_wii_bns(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
+    off_t bns_offset;
+    uint32_t info_offset = 0, data_offset = 0;
     uint32_t channel_info_offset_list_offset;
-	int channel_count;
-    int loop_flag;
-    uint16_t sample_rate;
+	int channels, loop_flag, sample_rate;
     uint32_t sample_count, loop_start;
 
-    /* check extension, case insensitive */
-    streamFile->get_name(streamFile,filename,sizeof(filename));
-    if (strcasecmp("bns",filename_extension(filename))) goto fail;
+    /* checks */
+    /* .bin: actual extension
+     * .bns: header id */
+    if (!check_extensions(sf, "bin,lbin,bns"))
+        goto fail;
 
-    // check header
-    BNS_offset = 0;
-    if (read_32bitBE(BNS_offset,streamFile) == 0x494D4435) // IMD5
-    {
-        // Skip IMD5 header if present
-        BNS_offset = 0x20;
+    bns_offset = 0;
+    if (is_id32be(bns_offset + 0x40, sf, "IMET")) {
+        /* regular .bnr, find sound.bin offset */
+        bns_offset = read_u32be(bns_offset + 0x44,sf);
+
+        /* tables, probably not ok for all cases */
+        bns_offset += read_u32be(bns_offset + 0x54,sf);
     }
 
-    if (read_32bitBE(BNS_offset+0x00,streamFile) != 0x424E5320) goto fail; // "BNS "
-    if ((uint32_t)read_32bitBE(BNS_offset+0x04,streamFile) != 0xFEFF0100u) goto fail;
+    if (is_id32be(bns_offset + 0x00, sf, "IMD5")) {
+        /* skip IMD5 header if present */
+        bns_offset += 0x20;
+    }
 
-    // find chunks, verify
+    if (!is_id32be(bns_offset + 0x00,sf, "BNS "))
+        goto fail;
+    if (read_u32be(bns_offset + 0x04,sf) != 0xFEFF0100u)
+        goto fail;
+
+    /* find chunks */
     {
-        // file size as claimed by header
-        uint32_t header_file_size = read_32bitBE(BNS_offset+0x08,streamFile);
-
-        uint32_t header_size = read_16bitBE(BNS_offset+0xc,streamFile);
-        uint16_t chunk_count = read_16bitBE(BNS_offset+0xe,streamFile);
-
+        uint32_t file_size   = read_u32be(bns_offset+0x08,sf);
+        uint32_t header_size = read_u16be(bns_offset+0x0c,sf);
+        uint16_t chunk_count = read_u16be(bns_offset+0x0e,sf);
         int i;
 
-        // assume BNS is the last thing in the file
-        if (header_file_size + BNS_offset != get_streamfile_size(streamFile))
+        /* assume BNS is the last thing in the file */
+        if (file_size + bns_offset != get_streamfile_size(sf))
             goto fail;
 
         for (i = 0; i < chunk_count; i++) {
-            uint32_t chunk_info_offset = BNS_offset+0x10+i*8;
+            uint32_t chunk_info_offset = bns_offset+0x10+i*0x08;
             uint32_t chunk_offset, chunk_size;
 
             // ensure chunk info is within header
-            if (chunk_info_offset+8 > BNS_offset+header_size) goto fail;
+            if (chunk_info_offset+8 > bns_offset+header_size) goto fail;
 
-            chunk_offset = BNS_offset + (uint32_t)read_32bitBE(chunk_info_offset,streamFile);
-            chunk_size = (uint32_t)read_32bitBE(chunk_info_offset+4,streamFile);
+            chunk_offset = bns_offset + (uint32_t)read_32bitBE(chunk_info_offset,sf);
+            chunk_size = (uint32_t)read_32bitBE(chunk_info_offset+4,sf);
 
             // ensure chunk is within file
-            if (chunk_offset < BNS_offset+header_size ||
-                chunk_offset+chunk_size > BNS_offset+header_file_size) goto fail;
+            if (chunk_offset < bns_offset+header_size ||
+                chunk_offset+chunk_size > bns_offset+file_size) goto fail;
 
             // ensure chunk size in header matches that listed in chunk
             // Note: disabled for now, as the Homebrew Channel BNS has a DATA
             // chunk that doesn't include the header size
-            //if ((uint32_t)read_32bitBE(chunk_offset+4,streamFile) != chunk_size) goto fail;
+            //if ((uint32_t)read_32bitBE(chunk_offset+4,sf) != chunk_size) goto fail;
 
             // handle each chunk type
-            switch (read_32bitBE(chunk_offset,streamFile)) {
+            switch (read_32bitBE(chunk_offset,sf)) {
                 case 0x494E464F:    // INFO
-                    info_offset = chunk_offset+8;
+                    info_offset = chunk_offset+0x08;
                     break;
                 case 0x44415441:    // DATA
-                    data_offset = chunk_offset+8;
+                    data_offset = chunk_offset+0x08;
                     break;
                 default:
                     goto fail;
@@ -82,72 +86,65 @@ VGMSTREAM * init_vgmstream_wii_bns(STREAMFILE *streamFile) {
     /* parse out basic stuff in INFO */
     {
         /* only seen this zero, specifies DSP format? */
-        if (read_8bit(info_offset+0x00,streamFile) != 0) goto fail;
+        if (read_8bit(info_offset+0x00,sf) != 0) goto fail;
 
-        loop_flag = read_8bit(info_offset+0x01,streamFile);
-        channel_count = read_8bit(info_offset+0x02,streamFile);
+        loop_flag = read_8bit(info_offset+0x01,sf);
+        channels = read_8bit(info_offset+0x02,sf);
 
         /* only seen zero, padding? */
-        if (read_8bit(info_offset+0x03,streamFile) != 0) goto fail;
+        if (read_8bit(info_offset+0x03,sf) != 0) goto fail;
 
-        sample_rate = (uint16_t)read_16bitBE(info_offset+0x04,streamFile);
+        sample_rate = read_u16be(info_offset+0x04,sf);
 
         /* only seen this zero, padding? */
-        if (read_16bitBE(info_offset+0x06,streamFile) != 0) goto fail;
+        if (read_u16be(info_offset+0x06,sf) != 0) goto fail;
 
-        loop_start = read_32bitBE(info_offset+0x08,streamFile);
+        loop_start = read_s32be(info_offset+0x08,sf);
 
-        sample_count = read_32bitBE(info_offset+0x0c,streamFile);
+        sample_count = read_s32be(info_offset+0x0c,sf);
 
-        channel_info_offset_list_offset = info_offset + (uint32_t)read_32bitBE(info_offset+0x10,streamFile);
+        channel_info_offset_list_offset = info_offset + read_u32be(info_offset+0x10,sf);
     }
 
 	/* build the VGMSTREAM */
-    vgmstream = allocate_vgmstream(channel_count,loop_flag);
+    vgmstream = allocate_vgmstream(channels, loop_flag);
     if (!vgmstream) goto fail;
 
-	/* fill in the vital statistics */
-	vgmstream->channels = channel_count;
-    vgmstream->sample_rate = sample_rate;
-    vgmstream->coding_type = coding_NGC_DSP;
-    vgmstream->num_samples = sample_count;
-    vgmstream->layout_type = layout_none;
     vgmstream->meta_type = meta_WII_BNS;
+    vgmstream->sample_rate = sample_rate;
+    vgmstream->num_samples = sample_count;
+    vgmstream->loop_start_sample = loop_start;
+    vgmstream->loop_end_sample = sample_count;
 
-    if (loop_flag)
-    {
-        vgmstream->loop_start_sample = loop_start;
-        vgmstream->loop_end_sample = sample_count;
-    }
+    vgmstream->coding_type = coding_NGC_DSP;
+    vgmstream->layout_type = layout_none;
 
+    //todo cleanup
     /* open the file for reading */
     {
-        int i;
-        for (i=0;i<channel_count;i++) {
-            uint32_t channel_info_offset = info_offset + read_32bitBE(channel_info_offset_list_offset+4*i,streamFile);
-            uint32_t channel_data_offset = data_offset + (uint32_t)read_32bitBE(channel_info_offset+0,streamFile);
-            uint32_t channel_dsp_offset = info_offset + (uint32_t)read_32bitBE(channel_info_offset+4,streamFile);
-            int j;
+        int i, j;
+        for (i = 0; i < channels; i++) {
+            uint32_t channel_info_offset = info_offset + read_u32be(channel_info_offset_list_offset + 0x04*i,sf);
+            uint32_t channel_data_offset = data_offset + read_u32be(channel_info_offset + 0x00,sf);
+            uint32_t channel_dsp_offset  = info_offset + read_u32be(channel_info_offset + 0x04,sf);
 
             /* always been 0... */
-            if (read_32bitBE(channel_info_offset+8,streamFile) != 0) goto fail;
+            if (read_u32be(channel_info_offset + 0x8,sf) != 0) goto fail;
 
-            vgmstream->ch[i].streamfile = streamFile->open(streamFile,filename,STREAMFILE_DEFAULT_BUFFER_SIZE);
+            vgmstream->ch[i].streamfile = reopen_streamfile(sf, 0);
             if (!vgmstream->ch[i].streamfile) goto fail;
+            vgmstream->ch[i].channel_start_offset = vgmstream->ch[i].offset = channel_data_offset;
 
-            vgmstream->ch[i].channel_start_offset=
-                vgmstream->ch[i].offset=channel_data_offset;
-
-            for (j=0;j<16;j++)
-                vgmstream->ch[i].adpcm_coef[j] =
-                    read_16bitBE(channel_dsp_offset+j*2,streamFile);
+            for (j = 0; j < 16; j++) {
+                vgmstream->ch[i].adpcm_coef[j] = read_s16be(channel_dsp_offset + j*0x02,sf);
+            }
         }
     }
 
+    //if (!vgmstream_open_stream(vgmstream, sf, start_offset))
+    //    goto fail;
     return vgmstream;
-
-    /* clean up anything we may have opened */
 fail:
-    if (vgmstream) close_vgmstream(vgmstream);
+    close_vgmstream(vgmstream);
     return NULL;
 }
