0487cf54353d18beedfe333d6e1ffff51c56d410
bnnm <bananaman255@gmail.com>
2021-06-21 00:30:15 +0200

    Cleanup

--- src/meta/csmp.c
+++ src/meta/csmp.c
@@ -3,49 +3,49 @@
 
 
 /* CSMP - Retro Studios sample [Metroid Prime 3 (Wii), Donkey Kong Country Returns (Wii)] */
-VGMSTREAM * init_vgmstream_csmp(STREAMFILE *streamFile) {
-    VGMSTREAM * vgmstream = NULL;
+VGMSTREAM* init_vgmstream_csmp(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
     off_t start_offset, first_offset = 0x08, chunk_offset;
-    int loop_flag, channel_count;
+    int loop_flag, channels;
 
 
     /* checks */
-    if (!check_extensions(streamFile, "csmp"))
+    if (!check_extensions(sf, "csmp"))
         goto fail;
-    if (read_32bitBE(0x00, streamFile) != 0x43534D50) /* "CSMP" */
+    if (!is_id32be(0x00, sf, "CSMP"))
         goto fail;
-    if (read_32bitBE(0x04, streamFile) != 1)  /* version? */
+    if (read_u32be(0x04, sf) != 1)
         goto fail;
 
-    if (!find_chunk(streamFile, 0x44415441,first_offset,0, &chunk_offset,NULL, 1, 0)) /*"DATA"*/
+    if (!find_chunk(sf, 0x44415441,first_offset,0, &chunk_offset,NULL, 1, 0)) /*"DATA"*/
         goto fail;
 
     /* contains standard DSP header, but somehow some validations (start/loop ps)
      * don't seem to work, so no point to handle as standard DSP */
 
-    channel_count = 1;
-    loop_flag = read_16bitBE(chunk_offset+0x0c,streamFile);
+    channels = 1;
+    loop_flag = read_s16be(chunk_offset+0x0c,sf);
     start_offset = chunk_offset + 0x60;
 
 
     /* build the VGMSTREAM */
-    vgmstream = allocate_vgmstream(channel_count,loop_flag);
+    vgmstream = allocate_vgmstream(channels, loop_flag);
     if (!vgmstream) goto fail;
 
     vgmstream->meta_type = meta_CSMP;
-    vgmstream->sample_rate = read_32bitBE(chunk_offset+0x08,streamFile);
-    vgmstream->num_samples = read_32bitBE(chunk_offset+0x00,streamFile);
-    vgmstream->loop_start_sample = dsp_nibbles_to_samples(read_32bitBE(chunk_offset+0x10,streamFile));
-    vgmstream->loop_end_sample =  dsp_nibbles_to_samples(read_32bitBE(chunk_offset+0x14,streamFile))+1;
+    vgmstream->sample_rate = read_s32be(chunk_offset+0x08,sf);
+    vgmstream->num_samples = read_s32be(chunk_offset+0x00,sf);
+    vgmstream->loop_start_sample = dsp_nibbles_to_samples(read_u32be(chunk_offset+0x10,sf));
+    vgmstream->loop_end_sample   = dsp_nibbles_to_samples(read_u32be(chunk_offset+0x14,sf)) + 1;
     if (vgmstream->loop_end_sample > vgmstream->num_samples) /* ? */
         vgmstream->loop_end_sample = vgmstream->num_samples;
 
     vgmstream->coding_type = coding_NGC_DSP;
     vgmstream->layout_type = layout_none;
-    dsp_read_coefs_be(vgmstream, streamFile, chunk_offset+0x1c, 0x00);
-    dsp_read_hist_be(vgmstream, streamFile, chunk_offset+0x40, 0x00);
+    dsp_read_coefs_be(vgmstream, sf, chunk_offset+0x1c, 0x00);
+    dsp_read_hist_be(vgmstream, sf, chunk_offset+0x40, 0x00);
 
-    if (!vgmstream_open_stream(vgmstream,streamFile,start_offset))
+    if (!vgmstream_open_stream(vgmstream, sf, start_offset))
         goto fail;
     return vgmstream;
 
--- src/meta/msf_banpresto.c
+++ src/meta/msf_banpresto.c
@@ -2,61 +2,61 @@
 #include "../coding/coding.h"
 
 /* WMSF - Banpresto MSFx wrapper [Dai-2-Ji Super Robot Taisen OG: The Moon Dwellers (PS3)] */
-VGMSTREAM * init_vgmstream_msf_banpresto_wmsf(STREAMFILE *streamFile) {
-    VGMSTREAM *vgmstream = NULL;
-    STREAMFILE *temp_streamFile = NULL;
+VGMSTREAM* init_vgmstream_msf_banpresto_wmsf(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
+    STREAMFILE* temp_sf = NULL;
     off_t subfile_offset = 0x10;
-    size_t subfile_size = get_streamfile_size(streamFile) - subfile_offset;
+    size_t subfile_size = get_streamfile_size(sf) - subfile_offset;
 
 
     /* checks */
-    if ( !check_extensions(streamFile,"msf"))
+    if (!check_extensions(sf,"msf"))
         goto fail;
-    if (read_32bitBE(0x00,streamFile) != 0x574D5346) /* "WMSF" */
+    if (!is_id32be(0x00,sf,"WMSF"))
         goto fail;
     /* 0x04: size, 0x08: flags? 0x0c: null? */
 
-    temp_streamFile = setup_subfile_streamfile(streamFile, subfile_offset,subfile_size, NULL);
-    if (!temp_streamFile) goto fail;
+    temp_sf = setup_subfile_streamfile(sf, subfile_offset,subfile_size, NULL);
+    if (!temp_sf) goto fail;
 
-    vgmstream = init_vgmstream_msf(temp_streamFile);
+    vgmstream = init_vgmstream_msf(temp_sf);
     if (!vgmstream) goto fail;
 
-    close_streamfile(temp_streamFile);
+    close_streamfile(temp_sf);
     return vgmstream;
 
 fail:
-    close_streamfile(temp_streamFile);
+    close_streamfile(temp_sf);
     close_vgmstream(vgmstream);
     return NULL;
 }
 
 /* 2MSF - Banpresto RIFF wrapper [Dai-2-Ji Super Robot Taisen OG: The Moon Dwellers (PS4)] */
-VGMSTREAM * init_vgmstream_msf_banpresto_2msf(STREAMFILE *streamFile) {
-    VGMSTREAM *vgmstream = NULL;
-    STREAMFILE *temp_streamFile = NULL;
+VGMSTREAM* init_vgmstream_msf_banpresto_2msf(STREAMFILE *sf) {
+    VGMSTREAM* vgmstream = NULL;
+    STREAMFILE*temp_sf = NULL;
     off_t subfile_offset = 0x14;
-    size_t subfile_size = get_streamfile_size(streamFile) - subfile_offset;
+    size_t subfile_size = get_streamfile_size(sf) - subfile_offset;
 
 
     /* checks */
-    if ( !check_extensions(streamFile,"at9"))
+    if ( !check_extensions(sf,"at9"))
         goto fail;
-    if (read_32bitBE(0x00,streamFile) != 0x324D5346) /* "2MSF" */
+    if (!is_id32be(0x00,sf,"2MSF"))
         goto fail;
     /* 0x04: size, 0x08: flags? 0x0c: null?, 0x10: 0x01? (BE values even though RIFF is LE) */
 
-    temp_streamFile = setup_subfile_streamfile(streamFile, subfile_offset,subfile_size, NULL);
-    if (!temp_streamFile) goto fail;
+    temp_sf = setup_subfile_streamfile(sf, subfile_offset,subfile_size, NULL);
+    if (!temp_sf) goto fail;
 
-    vgmstream = init_vgmstream_riff(temp_streamFile);
+    vgmstream = init_vgmstream_riff(temp_sf);
     if (!vgmstream) goto fail;
 
-    close_streamfile(temp_streamFile);
+    close_streamfile(temp_sf);
     return vgmstream;
 
 fail:
-    close_streamfile(temp_streamFile);
+    close_streamfile(temp_sf);
     close_vgmstream(vgmstream);
     return NULL;
 }
