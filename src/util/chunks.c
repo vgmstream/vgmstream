374e1d3c3264593b1ebbdaaacbd0d8660470a7d9
bnnm <bananaman255@gmail.com>
2021-08-26 19:52:19 +0200

    utils: chunk parsing helper

---
+++ src/util/chunks.c
@@ -0,0 +1,35 @@
+#include "chunks.h"
+//#include "log.h"
+
+
+int next_chunk(chunk_t* chunk, STREAMFILE* sf) {
+    uint32_t (*read_u32type)(off_t,STREAMFILE*) = !chunk->le_type ? read_u32be : read_u32le;
+    uint32_t (*read_u32size)(off_t,STREAMFILE*) = chunk->be_size ? read_u32be : read_u32le;
+
+    if (chunk->max == 0)
+        chunk->max = get_streamfile_size(sf);
+
+    if (chunk->current >= chunk->max)
+        return 0;
+    /* can be used to signal "stop" */
+    if (chunk->current < 0)
+        return 0;
+
+    chunk->type = read_u32type(chunk->current + 0x00,sf);
+    chunk->size = read_u32size(chunk->current + 0x04,sf);
+
+    chunk->offset = chunk->current + 0x04 + 0x04;
+    chunk->current += chunk->full_size ? chunk->size : 0x08 + chunk->size;
+    //;VGM_LOG("CHUNK: %x, %x, %x\n", dc.offset, chunk->type, chunk->size);
+
+    /* read past data */
+    if (chunk->type == 0xFFFFFFFF || chunk->size == 0xFFFFFFFF)
+        return 0;
+
+    /* empty chunk with 0 size, seen in some formats (XVAG uses it as end marker, Wwise doesn't) */
+    if (chunk->type == 0 || chunk->size == 0)
+        return 0;
+
+    /* more chunks remain */
+    return 1;
+}
