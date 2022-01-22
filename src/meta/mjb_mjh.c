098937ffebbbdc28ebfd1b76e81fd7f99f5bd261
bnnm <bananaman255@gmail.com>
2022-01-22 22:56:35 +0100

    .mjh+mjh cleanup

--- src/meta/mjb_mjh.c
+++ src/meta/mjb_mjh.c
@@ -5,18 +5,33 @@
 VGMSTREAM* init_vgmstream_mjb_mjh(STREAMFILE* sf) {
     VGMSTREAM* vgmstream = NULL;
     STREAMFILE* sh = NULL;
+    STREAMFILE* sb = NULL;
     off_t start_offset = 0, header_offset = 0;
     size_t data_size, frame_size, frame_count;
     int channels, loop_flag, sample_rate;
     int total_subsongs, target_subsong = sf->stream_index;
 
 
+    /* for plugins that start with .mjb */
+    if (check_extensions(sf,"mjb")) {
+        sh = open_streamfile_by_ext(sf, "mjh");
+        if (!sh) goto fail;
+    }
+    else {
+        sh = sf;
+    }
+
     /* checks */
+    /* "base" header check only has total subsongs, check expected header size */
+    if (read_s32le(0x00,sh) * 0x40 + 0x40 != get_streamfile_size(sh))
+        goto fail;
+    if (read_u32le(0x10,sh) != 0 || read_u32le(0x20,sh) != 0 || read_u32le(0x30,sh) != 0) /* padding until 0x40 */
+        goto fail;
+
+    /* only body for now */
     if (!check_extensions(sf, "mjb"))
         goto fail;
 
-    sh = open_streamfile_by_ext(sf, "mjh");
-    if (!sh) goto fail;
 
     /* parse entries */
     {
@@ -42,8 +57,6 @@ VGMSTREAM* init_vgmstream_mjb_mjh(STREAMFILE* sf) {
             goto fail;
     }
 
-    VGM_LOG("3: %lx\n", start_offset);
-
     /* also see MIB+MIH (same thing but this excludes padding stuff) */
     if (read_u32le(header_offset + 0x00,sh) != 0x40)
         goto fail;
@@ -54,6 +67,16 @@ VGMSTREAM* init_vgmstream_mjb_mjh(STREAMFILE* sf) {
 
     data_size  = frame_count * frame_size * channels;
     loop_flag = 0;
+
+
+    /* for plugins that start with .mjh (and don't check extensions) */
+    if (sf == sh) {
+        sb = open_streamfile_by_ext(sf, "mjb");
+        if (!sb) goto fail;
+    }
+    else {
+        sb = sf;
+    }
 
 
     /* build the VGMSTREAM */
@@ -71,13 +94,15 @@ VGMSTREAM* init_vgmstream_mjb_mjh(STREAMFILE* sf) {
     vgmstream->layout_type = layout_interleave;
     vgmstream->interleave_block_size = frame_size;
 
-    if (!vgmstream_open_stream(vgmstream, sf, start_offset))
+    if (!vgmstream_open_stream(vgmstream, sb, start_offset))
         goto fail;
-    close_streamfile(sh);
+    if (sf != sh) close_streamfile(sh);
+    if (sf != sb) close_streamfile(sb);
     return vgmstream;
 
 fail:
-    close_streamfile(sh);
+    if (sf != sh) close_streamfile(sh);
+    if (sf != sb) close_streamfile(sb);
     close_vgmstream(vgmstream);
     return NULL;
 }
