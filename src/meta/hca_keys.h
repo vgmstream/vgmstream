8bf5ef7c214bba4c1c16579c567ddffc192335e8
bnnm <bananaman255@gmail.com>
2022-03-29 23:45:04 +0200

    Add HCA key

--- src/meta/hca_keys.h
+++ src/meta/hca_keys.h
@@ -858,6 +858,7 @@ static const hcakey_info hcakey_list[] = {
         {0x0a5d0fc8cc5c4502},   // Sng018
         {0x198ea1a17416050b},   // Sng019
         {0x2aa3b8abad207a1e},   // Sng020
+        {0x4ee10a3e3bb19e57},   // Sng021
         {0x08ad2fe12c79bca9},   // Sng022
         {0x18488992b1632ef5},   // Sng023
         {0x1175edbbacc1fc18},   // Sng024
