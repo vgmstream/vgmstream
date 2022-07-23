84a1053fd3affc216340bf334be8cf603aacd9ad
bnnm <bananaman255@gmail.com>
2022-07-23 15:15:16 +0200

    cleanup

--- src/meta/apc.c
+++ src/meta/apc.c
@@ -2,43 +2,43 @@
 #include "../coding/coding.h"
 
 /* APC - from Cryo games [MegaRace 3 (PC)] */
-VGMSTREAM * init_vgmstream_apc(STREAMFILE *streamFile) {
-    VGMSTREAM * vgmstream = NULL;
-    off_t start_offset;
-    size_t data_size;
-    int loop_flag, channel_count;
+VGMSTREAM* init_vgmstream_apc(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
+    uint32_t start_offset, data_size;
+    int loop_flag, channels, sample_rate;
 
 
     /* checks */
-    if ( !check_extensions(streamFile,"apc") )
+    if (!is_id32be(0x00,sf, "CRYO"))
         goto fail;
-    if (read_32bitBE(0x00,streamFile) != 0x4352594F) /* "CRYO" */
+    if (!is_id32be(0x04,sf, "_APC"))
         goto fail;
-    if (read_32bitBE(0x04,streamFile) != 0x5F415043) /* "_APC" */
-        goto fail;
-  //if (read_32bitBE(0x08,streamFile) != 0x312E3230) /* "1.20" */
+  //if (!is_id32be(0x04,sf, "1.20"))
   //    goto fail;
 
-    /* 0x14/18: L/R hist sample? */
+    if (!check_extensions(sf,"apc"))
+        goto fail;
 
-    start_offset = 0x20;
-    data_size = get_streamfile_size(streamFile) - start_offset;
-    channel_count = read_32bitLE(0x1c,streamFile) == 0 ? 1 : 2;
+    sample_rate = read_s32le(0x10,sf);
+    /* 0x14/18: L/R hist sample? */
+    channels = read_s32le(0x1c,sf) == 0 ? 1 : 2;
     loop_flag = 0;
+    start_offset = 0x20;
+    data_size = get_streamfile_size(sf) - start_offset;
 
 
     /* build the VGMSTREAM */
-    vgmstream = allocate_vgmstream(channel_count,loop_flag);
+    vgmstream = allocate_vgmstream(channels, loop_flag);
     if (!vgmstream) goto fail;
 
     vgmstream->meta_type = meta_APC;
-    vgmstream->sample_rate = read_32bitLE(0x10,streamFile);
-    vgmstream->num_samples = ima_bytes_to_samples(data_size,channel_count);
+    vgmstream->sample_rate = sample_rate;
+    vgmstream->num_samples = ima_bytes_to_samples(data_size, channels);
 
     vgmstream->coding_type = coding_IMA;
     vgmstream->layout_type = layout_none;
 
-    if ( !vgmstream_open_stream(vgmstream, streamFile, start_offset) )
+    if (!vgmstream_open_stream(vgmstream, sf, start_offset))
         goto fail;
     return vgmstream;
 
