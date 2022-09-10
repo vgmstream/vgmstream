dc27ebe121efc004c4ef44047bb0dc368bddff69
NicknineTheEagle <nik18.1995@gmail.com>
2022-09-10 05:42:48 +0300

    Formatting

--- src/meta/esf.c
+++ src/meta/esf.c
@@ -22,38 +22,42 @@ VGMSTREAM* init_vgmstream_esf(STREAMFILE* sf) {
     version = read_u8(0x03, sf);
     pcm_size = read_u32le(0x04, sf);
 
-    if (version == 3) {
-        /* Disney's Hercules */
-        sample_rate = read_u32le(0x08, sf);
-        loop_flag = read_u8(0x0c, sf);
-        bps_flag = read_u8(0x0d, sf);
+    switch (version) {
+        case 3:
+            /* Disney's Hercules */
+            sample_rate = read_u32le(0x08, sf);
+            loop_flag = read_u8(0x0c, sf);
+            bps_flag = read_u8(0x0d, sf);
 
-        //bps = bps_flag ? 8 : 16;
-        bps = 16;
-        start_offset = 0x10;
-    } else if (version == 6) {
-        /* Mortal Kombat 4 */
-        bps_flag = pcm_size & 0x20000000;
-        hq_flag = pcm_size & 0x40000000;
-        loop_flag = pcm_size & 0x80000000;
-        pcm_size &= 0x1FFFFFFF;
+            //bps = bps_flag ? 8 : 16;
+            bps = 16;
+            start_offset = 0x10;
+            break;
+        case 6:
+            /* Mortal Kombat 4 */
+            bps_flag = pcm_size & 0x20000000;
+            hq_flag = pcm_size & 0x40000000;
+            loop_flag = pcm_size & 0x80000000;
+            pcm_size &= 0x1FFFFFFF;
 
-        bps = bps_flag ? 16 : 8;
-        sample_rate = hq_flag ? 22050 : 11025;
-        start_offset = 0x08;
-    } else if (version == 8) {
-        /* Disney's Tarzan, Hydro Thunder */
-        bps_flag = pcm_size & 0x10000000;
-        hq_flag = pcm_size & 0x20000000;
-        loop_flag = pcm_size & 0x40000000;
-        codec_flag = pcm_size & 0x80000000;
-        pcm_size &= 0x0FFFFFFF;
+            bps = bps_flag ? 16 : 8;
+            sample_rate = hq_flag ? 22050 : 11025;
+            start_offset = 0x08;
+            break;
+        case 8:
+            /* Disney's Tarzan, Hydro Thunder */
+            bps_flag = pcm_size & 0x10000000;
+            hq_flag = pcm_size & 0x20000000;
+            loop_flag = pcm_size & 0x40000000;
+            codec_flag = pcm_size & 0x80000000;
+            pcm_size &= 0x0FFFFFFF;
 
-        bps = bps_flag ? 16 : 8;
-        sample_rate = hq_flag ? 22050 : 11025;
-        start_offset = 0x08;
-    } else {
-        goto fail;
+            bps = bps_flag ? 16 : 8;
+            sample_rate = hq_flag ? 22050 : 11025;
+            start_offset = 0x08;
+            break;
+        default:
+            goto fail;
     }
 
     channels = 1; /* mono only */
@@ -64,20 +68,26 @@ VGMSTREAM* init_vgmstream_esf(STREAMFILE* sf) {
 
     vgmstream->meta_type = meta_ESF;
     vgmstream->sample_rate = sample_rate;
-    if (version == 3) {
-        vgmstream->coding_type = coding_DVI_IMA;
-    } else if (version == 6) {
-        vgmstream->coding_type = (bps == 8) ? coding_PCM8_U : coding_DVI_IMA;
-    } else if (version == 8) {
-        if (bps == 8) {
-            vgmstream->coding_type = coding_PCM8_U;
-        } else {
-            vgmstream->coding_type = codec_flag ? coding_DVI_IMA : coding_PCM16LE;
-        }
-    }
     vgmstream->num_samples = pcm_bytes_to_samples(pcm_size, channels, bps);
     vgmstream->loop_start_sample = 0;
     vgmstream->loop_end_sample = vgmstream->num_samples;
+    vgmstream->layout_type = layout_none;
+
+    switch (version) {
+        case 3:
+            vgmstream->coding_type = coding_DVI_IMA;
+            break;
+        case 6:
+            vgmstream->coding_type = (bps == 8) ? coding_PCM8_U : coding_DVI_IMA;
+            break;
+        case 8:
+            if (bps == 8) {
+                vgmstream->coding_type = coding_PCM8_U;
+            } else {
+                vgmstream->coding_type = codec_flag ? coding_DVI_IMA : coding_PCM16LE;
+            }
+            break;
+    }
 
     if (!vgmstream_open_stream(vgmstream, sf, start_offset))
         goto fail;
