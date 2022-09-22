47381e7e3db5c09575b6110fd49e279a90847fa1
bnnm <bananaman255@gmail.com>
2022-09-22 23:19:46 +0200

    Reduce internal path limit for more common cases

--- src/vgmstream.h
+++ src/vgmstream.h
@@ -7,7 +7,10 @@
 
 /* reasonable limits */
 enum { 
-    PATH_LIMIT = 32768,
+    /* Windows generally only allows 260 chars in path, but other OSs have higher limits, and we handle
+     * UTF-8 (that typically uses 2-bytes for common non-latin codepages) plus player may append protocols
+     * to paths, so it should be a bit higher. Most people wouldn't use huge paths though. */
+    PATH_LIMIT = 4096, /* (256 * 8) * 2 = ~max_path * (other_os+extra) * codepage_bytes */
     STREAM_NAME_SIZE = 255,
     VGMSTREAM_MAX_CHANNELS = 64,
     VGMSTREAM_MIN_SAMPLE_RATE = 300, /* 300 is Wwise min */
