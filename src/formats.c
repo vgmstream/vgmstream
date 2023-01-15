9156bbffbf1f8402d6e7c25ff72bdcea34ae06a4
bnnm <bananaman255@gmail.com>
2023-01-15 18:44:00 +0100

    Add .binka extension (via FFmpeg)

--- src/formats.c
+++ src/formats.c
@@ -110,6 +110,7 @@ static const char* extension_list[] = {
     "bik",
     "bika", //fake extension for .bik (to be removed)
     "bik2",
+    "binka", //FFmpeg/not parsed (BINK AUDIO)
     //"bin", //common
     "bk2",
     "bkr",  //txth/reserved [P.N.03 (GC), Viewtiful Joe (GC)]
