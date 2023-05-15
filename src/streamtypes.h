a211dcf9b0747bd3fc53f028242326e986970bd0
bnnm <bananaman255@gmail.com>
2023-05-15 16:52:09 +0200

    cleanup: tweaks

--- src/streamtypes.h
+++ src/streamtypes.h
@@ -1,10 +1,11 @@
 /*
  * streamtypes.h - widely used type definitions
  */
-
-
 #ifndef _STREAMTYPES_H
 #define _STREAMTYPES_H
+
+#include <stddef.h> //size_t
+#include <stdbool.h> //bool
 
 #ifdef _MSC_VER
 /* Common versions:
@@ -15,25 +16,24 @@
  * - 1900: VS2015
  * - 1920: VS2019 */
 
-#if (_MSC_VER >= 1600)
+  #if (_MSC_VER >= 1600)
+    #include <stdint.h>
+  #else
+    #include <pstdint.h>
+  #endif
 
-#include <stdint.h>
+  #if (_MSC_VER < 1800) && !defined(__cplusplus)
+  #define inline __inline
+  #endif
+
+  #define strcasecmp _stricmp
+  #define strncasecmp _strnicmp
+
+  #if (_MSC_VER < 1900)
+  #define snprintf _snprintf
+  #endif
 #else
-#include <pstdint.h>
-#endif
 
-#if (_MSC_VER < 1800) && !defined(__cplusplus)
-#define inline __inline
-#endif
-
-#define strcasecmp _stricmp
-#define strncasecmp _strnicmp
-
-#if (_MSC_VER < 1900)
-#define snprintf _snprintf
-#endif
-
-#else
 #include <stdint.h>
 
 #endif /* _MSC_VER */
