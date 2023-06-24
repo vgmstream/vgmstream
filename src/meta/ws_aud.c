c65c454d83eb99ebf75e1546c3658d3994caeeab
bnnm <bananaman255@gmail.com>
2023-06-24 16:58:44 +0200

    cleandup: ws_aud

--- src/meta/ws_aud.c
+++ src/meta/ws_aud.c
@@ -4,128 +4,108 @@
 
 /* Westwood Studios .aud (WS-AUD) */
 
-VGMSTREAM * init_vgmstream_ws_aud(STREAMFILE *streamFile) {
-    VGMSTREAM * vgmstream = NULL;
-    char filename[PATH_LIMIT];
-
+VGMSTREAM* init_vgmstream_ws_aud(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
     coding_t coding_type = -1;
     off_t format_offset;
-
-    int channel_count;
-    int new_type = 0;   /* if 0 is old type */
-
+    int channels;
+    bool new_type = false;
     int bytes_per_sample = 0;
 
-    /* check extension, case insensitive */
-    streamFile->get_name(streamFile,filename,sizeof(filename));
-    if (strcasecmp("aud",filename_extension(filename))) goto fail;
+
+    /* checks **/
+    if (!check_extensions(sf, "aud") )
+        return NULL;
 
     /* check for 0x0000DEAF chunk marker for first chunk */
-    if (read_32bitLE(0x10,streamFile)==0x0000DEAF) {    /* new */
-        new_type = 1;
-    } else if (read_32bitLE(0x0C,streamFile)==0x0000DEAF) { /* old */
-        new_type = 0;
-    } else goto fail;
+    if (read_u32le(0x10,sf) == 0x0000DEAF) {    /* new */
+        new_type = true;
+        format_offset = 0x0A;
+    }
+    else if (read_u32le(0x0C,sf) == 0x0000DEAF) { /* old */
+        new_type = false;
+        format_offset = 0x06;
+    }
+    else {
+        return NULL;
+    }
 
-    if (new_type)
-        format_offset = 0xa;
+    /* blocked format with a mini-header */
+
+    if (read_u8(format_offset + 0x00, sf) & 1)
+        channels = 2;
     else
-        format_offset = 0x6;
+        channels = 1;
 
-    /* get channel count */
-    if (read_8bit(format_offset,streamFile) & 1)
-        channel_count = 2;
-    else
-        channel_count = 1;
+    if (channels == 2)
+        goto fail; /* not seen */
 
-    if (channel_count == 2) goto fail; /* TODO: not yet supported (largely
-                                          because not yet seen) */
-
-    /* get output format */
-    if (read_8bit(format_offset+1,streamFile) & 2)
+    if (read_u8(format_offset + 0x01,sf) & 2)
         bytes_per_sample = 2;
     else
         bytes_per_sample = 1;
 
     /* check codec type */
-    switch (read_8bit(format_offset+1,streamFile)) {
+    switch (read_u8(format_offset + 0x01,sf)) {
         case 1:     /* Westwood custom */
             coding_type = coding_WS;
-            /* shouldn't happen? */
-            if (bytes_per_sample != 1) goto fail;
+            if (bytes_per_sample != 1) goto fail; /* shouldn't happen? */
             break;
         case 99:    /* IMA ADPCM */
             coding_type = coding_IMA_int;
             break;
         default:
             goto fail;
-            break;
     }
 
     /* build the VGMSTREAM */
-
-    vgmstream = allocate_vgmstream(channel_count,0);
+    vgmstream = allocate_vgmstream(channels, 0);
     if (!vgmstream) goto fail;
 
-    /* fill in the vital statistics */
     if (new_type) {
-        vgmstream->num_samples = read_32bitLE(0x06,streamFile)/bytes_per_sample/channel_count;
-    } else {
+        vgmstream->num_samples = read_32bitLE(0x06,sf)/bytes_per_sample/channels;
+    }
+    else {
         /* Doh, no output size in old type files. We have to read through the
          * file looking at chunk headers! Crap! */
         int32_t out_size = 0;
         off_t current_offset = 0x8;
-        off_t file_size = get_streamfile_size(streamFile);
+        off_t file_size = get_streamfile_size(sf);
 
         while (current_offset < file_size) {
             int16_t chunk_size;
-            chunk_size = read_16bitLE(current_offset,streamFile);
-            out_size += read_16bitLE(current_offset+2,streamFile);
+            chunk_size = read_16bitLE(current_offset,sf);
+            out_size += read_16bitLE(current_offset+2,sf);
             /* while we're here might as well check for valid chunks */
-            if (read_32bitLE(current_offset+4,streamFile) != 0x0000DEAF) goto fail;
+            if (read_32bitLE(current_offset+4,sf) != 0x0000DEAF) goto fail;
             current_offset+=8+chunk_size;
         }
 
-        vgmstream->num_samples = out_size/bytes_per_sample/channel_count;
+        vgmstream->num_samples = out_size/bytes_per_sample/channels;
     }
     
     /* they tend to not actually have data for the last odd sample */
     if (vgmstream->num_samples & 1) vgmstream->num_samples--;
-    vgmstream->sample_rate = (uint16_t)read_16bitLE(0x00,streamFile);
+    vgmstream->sample_rate = (uint16_t)read_16bitLE(0x00,sf);
 
     vgmstream->coding_type = coding_type;
     if (new_type) {
         vgmstream->meta_type = meta_WS_AUD;
-    } else {
-        vgmstream->meta_type = meta_WS_AUD_old;
     }
 
     vgmstream->layout_type = layout_blocked_ws_aud;
 
-    /* open the file for reading by each channel */
-    {
-        int i;
-        STREAMFILE * file;
+    if (!vgmstream_open_stream(vgmstream, sf, 0x00) )
+        goto fail;
 
-        file = streamFile->open(streamFile,filename,
-                STREAMFILE_DEFAULT_BUFFER_SIZE);
-        if (!file) goto fail;
-
-        for (i=0;i<channel_count;i++) {
-            vgmstream->ch[i].streamfile = file;
-        }
-    }
-
-    /* start me up */
     if (new_type) {
-        block_update_ws_aud(0xc,vgmstream);
+        block_update(0x0c, vgmstream);
     } else {
-        block_update_ws_aud(0x8,vgmstream);
+        block_update(0x08, vgmstream);
     }
 
     return vgmstream;
 
-    /* clean up anything we may have opened */
 fail:
     if (vgmstream) close_vgmstream(vgmstream);
     return NULL;
