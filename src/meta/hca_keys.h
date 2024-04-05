a09c401321c0eae09963d571ed5cb2ae374c9e21
sonicdemo <121387979+sonicdemo@users.noreply.github.com>
2024-04-06 01:06:36 +0200

    Add new hca key for Puyo Puyo Puzzle Pop (iOS) (#1509)
    Add HCA key

--- src/meta/hca_keys.h
+++ src/meta/hca_keys.h
@@ -1305,6 +1305,9 @@ static const hcakey_info hcakey_list[] = {
         // Jujutsu Kaisen: Cursed Clash (multi)
         {984703514306706654},    // DAA5EA10B547CDE
 
+        // Puyo Puyo Puzzle Pop (iOS)
+        {9999},                  // 000000000000270F
+
 };
 
 #endif/*_HCA_KEYS_H_*/
