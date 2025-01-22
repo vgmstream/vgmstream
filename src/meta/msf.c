36545010737a9646037626d4026c4a18acb8f597
bnnm <bananaman255@gmail.com>
2025-01-22 22:49:46 +0100

    Fix some .msf loops being slightly off

--- src/meta/msf.c
+++ src/meta/msf.c
@@ -11,6 +11,15 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
 
 
     /* checks */
+    if ((read_u32be(0x00,sf) & 0xffffff00) != get_id32be("MSF\0"))
+        return NULL;
+    // "MSF" + n.n version:
+    // - 0x01: Megazone 23: Aoi Garland (PS3)
+    // - 0x02: Switchball (PS3)
+    // - 0x30 ('0'): ?
+    // - 0x35 ('5'): SDKs
+    // - 0x43 ('C'): latest/most common
+
     /* .msf: standard
      * .msa: Sonic & Sega All-Stars Racing (PS3)
      * .at3: Silent Hill HD Collection (PS3), Z/X Zekkai no Crusade (PS3)
@@ -18,12 +27,7 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
      * .str: Pac-Man and the Ghostly Adventures (PS3)
      * .snd: HamsterBall (PS3) */
     if (!check_extensions(sf,"msf,msa,at3,mp3,str,snd"))
-        goto fail;
-
-    /* check header "MSF" + version-char, usually:
-     *  0x01, 0x02, 0x30="0", 0x35="5", 0x43="C" (last/most common version) */
-    if ((read_u32be(0x00,sf) & 0xffffff00) != 0x4D534600) /* "MSF\0" */
-        goto fail;
+        return NULL;
 
     start_offset = 0x40;
 
@@ -45,13 +49,11 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
      * 0x10 often goes with 0x01 but not always (Castlevania HoD); Malicious PS3 uses flag 0x2 instead */
     loop_flag = (flags != 0xffffffff) && ((flags & 0x01) || (flags & 0x02));
 
-    /* loop markers (marker N @ 0x18 + N*(4+4), but in practice only marker 0 is used) */
+    /* loop offset markers (marker N @ 0x18 + N*(4+4), but in practice only marker 0 is used) */
     if (loop_flag) {
         loop_start = read_u32be(0x18,sf);
         loop_end = read_u32be(0x1C,sf); /* loop duration */
         loop_end = loop_start + loop_end; /* usually equals data_size but not always */
-        if (loop_end > data_size) /* not seen */
-            loop_end = data_size;
     }
 
 
@@ -71,12 +73,11 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
             vgmstream->layout_type = layout_interleave;
             vgmstream->interleave_block_size = 0x02;
 
-            vgmstream->num_samples = pcm_bytes_to_samples(data_size, channels, 16);
+            vgmstream->num_samples = pcm16_bytes_to_samples(data_size, channels);
             if (loop_flag){
-                vgmstream->loop_start_sample = pcm_bytes_to_samples(loop_start, channels, 16);
-                vgmstream->loop_end_sample = pcm_bytes_to_samples(loop_end, channels, 16);
+                vgmstream->loop_start_sample = pcm16_bytes_to_samples(loop_start, channels);
+                vgmstream->loop_end_sample = pcm16_bytes_to_samples(loop_end, channels);
             }
-
             break;
         }
 
@@ -102,28 +103,34 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
         case 0x04:   /* ATRAC3 low (66 kbps, frame size 96, Joint Stereo) [Silent Hill HD (PS3)] */
         case 0x05:   /* ATRAC3 mid (105 kbps, frame size 152) [Atelier Rorona (PS3)] */
         case 0x06: { /* ATRAC3 high (132 kbps, frame size 192) [Tekken Tag Tournament HD (PS3)] */
-            int block_align, encoder_delay;
 
-            /* MSF skip samples: from tests with MSEnc and real files (ex. TTT2 eddy.msf v43, v01 demos) seems like 1162 is consistent.
-             * Atelier Rorona bt_normal01 needs it to properly skip the beginning garbage but usually doesn't matter.
-             * (note that encoder may add a fade-in with looping/resampling enabled but should be skipped) */
-            encoder_delay = 1024 + 69*2;
-            block_align   = (codec==4 ? 0x60 : (codec==5 ? 0x98 : 0xC0)) * vgmstream->channels;
-            vgmstream->num_samples = atrac3_bytes_to_samples(data_size, block_align) - encoder_delay;
-            if (vgmstream->sample_rate == -1) /* some MSFv1 (Digi World SP) */
-                vgmstream->sample_rate = 44100; /* voice tracks seems to use 44khz, not sure about other tracks */
+            /* some MSFv1 voices [Digi World SP (PS3)] */
+            if (vgmstream->sample_rate == -1)
+                vgmstream->sample_rate = 44100;
 
-            vgmstream->codec_data = init_ffmpeg_atrac3_raw(sf, start_offset,data_size, vgmstream->num_samples,vgmstream->channels,vgmstream->sample_rate, block_align, encoder_delay);
+            int block_align = (codec==4 ? 0x60 : (codec==5 ? 0x98 : 0xC0)) * vgmstream->channels;
+            vgmstream->num_samples = atrac3_bytes_to_samples(data_size, block_align);
+            vgmstream->loop_start_sample = atrac3_bytes_to_samples(loop_start, block_align);
+            vgmstream->loop_end_sample   = atrac3_bytes_to_samples(loop_end, block_align);
+
+            /* MSF skip samples: from MSEnc tests and real files (ex. TTT2 eddy.msf v43, v01 demos) seems like 1162 is consistent.
+             * Often doesn't matter but sometimes there is audible garbage [Atelier Rorona (PS3)-bt_normal01]
+             * However full loops use offset 0 to file end, so maybe decoder doesn't actually skip samples (like in MPEG).
+             * MSEnc accepts samples and will adjust loops somewhat to closest frame but is not accurate enough.
+             * Comparing vs other platforms loop start+end need to be in sync [Mamoru-kun wa Norowarette Shimatta! (PS3)]
+             * For now only remove samples if wouldn't mess up loops. */
+            int encoder_delay = 1024 + 69*2;
+            if (vgmstream->loop_flag && encoder_delay > vgmstream->loop_start_sample) {
+                encoder_delay = 0;
+            }
+            vgmstream->num_samples -= encoder_delay;
+            vgmstream->loop_start_sample -= encoder_delay;
+            vgmstream->loop_end_sample -= encoder_delay;
+
+            vgmstream->codec_data = init_ffmpeg_atrac3_raw(sf, start_offset,data_size, vgmstream->num_samples,vgmstream->channels, vgmstream->sample_rate, block_align, encoder_delay);
             if (!vgmstream->codec_data) goto fail;
             vgmstream->coding_type = coding_FFmpeg;
             vgmstream->layout_type = layout_none;
-
-            /* MSF loop/sample values are offsets so trickier to adjust but this seems correct */
-            if (loop_flag) {
-                /* set offset samples (offset 0 jumps to sample 0 > pre-applied delay, and offset end loops after sample end > adjusted delay) */
-                vgmstream->loop_start_sample = atrac3_bytes_to_samples(loop_start, block_align); //- encoder_delay
-                vgmstream->loop_end_sample   = atrac3_bytes_to_samples(loop_end, block_align) - encoder_delay;
-            }
 
             break;
         }
@@ -139,7 +146,7 @@ VGMSTREAM* init_vgmstream_msf(STREAMFILE* sf) {
             vgmstream->num_samples = mpeg_get_samples_clean(sf, start_offset, data_size, &loop_start, &loop_end, is_vbr);
             vgmstream->loop_start_sample = loop_start;
             vgmstream->loop_end_sample = loop_end;
-            /* MPEG here seems stripped from ID3/Xing headers, loops are frame offsets */
+            /* MSEnc seems to strip ID3/Xing headers, loops are frame offsets */
 
             /* encoder delay varies between 1152 (1f), 528, 576, etc; probably not actually skipped */
             break;
