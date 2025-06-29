306a0f7b015f3ecfa6c0a0c4b7b09ff4eb6e6682
bnnm <bananaman255@gmail.com>
2025-06-29 10:57:32 +0200

    Tweak Tantalus ADPCM

--- src/coding/tantalus_decoder.c
+++ src/coding/tantalus_decoder.c
@@ -2,13 +2,13 @@
 
 
 /* Decodes Tantalus TADC ADPCM codec, used in Saturn games.
- * Guessed based on other XA-style codecs values. */
+ * Reverse engineered from the exe (@ 0x06086d2 w/ 0x06010000 base address) */
 void decode_tantalus(VGMSTREAMCHANNEL* stream, sample_t* outbuf, int channelspacing, int32_t first_sample, int32_t samples_to_do) {
     uint8_t frame[0x10] = {0};
     off_t frame_offset;
-    int i, frames_in, sample_count = 0;
+    int frames_in, sample_count = 0;
     size_t bytes_per_frame, samples_per_frame;
-    int shift, filter, coef1, coef2;
+
     int32_t hist1 = stream->adpcm_history1_32;
     int32_t hist2 = stream->adpcm_history2_32;
 
@@ -22,39 +22,31 @@ void decode_tantalus(VGMSTREAMCHANNEL* stream, sample_t* outbuf, int channelspac
     /* parse frame header */
     frame_offset = stream->offset + bytes_per_frame*frames_in;
     read_streamfile(frame, frame_offset, bytes_per_frame, stream->streamfile); /* ignore EOF errors */
-    filter = (frame[0x00] >> 4) & 0xf; /* 0 in tested files */
-    shift = (frame[0x00] >> 0) & 0xf;
-    if (filter != 0) {
-        VGM_LOG_ONCE("TANTALUS: unknown filter\n");
-        coef1 = 64;
-        coef2 = 64; /* will sound horrid and hopefully reported */
-    }
-    else {
-        coef1 = 64;
-        coef2 = 0;
-    }
+
+    // there is no XA filter select code but upper 4 bits seem to be always 0
+    int shift = (frame[0x00] >> 0);
 
 
     /* decode nibbles */
-    for (i = first_sample; i < first_sample + samples_to_do; i++) {
+    for (int i = first_sample; i < first_sample + samples_to_do; i++) {
         uint8_t nibbles = frame[0x01 + i/2];
         int32_t sample;
 
-        sample = i&1 ? /* low nibble first */
+        int8_t code = i&1 ? /* low nibble first */
                 get_high_nibble_signed(nibbles) :
                 get_low_nibble_signed(nibbles);
-        sample = sample << (shift + 6);
-        sample = (sample + (hist1 * coef1) + (hist2 * coef2)) >> 6;
+
+        // calc is done via [code][shift] = delta LUT in OG code (perhaps because SH2 can only do 1/2/4 shifts)
+        // basically equivalent to an XA codec with coef1 = 1.0, coef2 = 0.0
+        sample = hist1 + (code << shift);
 
         outbuf[sample_count] = clamp16(sample);
         sample_count += channelspacing;
 
-        hist2 = hist1;
         hist1 = sample;
     }
 
     stream->adpcm_history1_32 = hist1;
-    stream->adpcm_history2_32 = hist2;
 }
 
 int32_t tantalus_bytes_to_samples(size_t bytes, int channels) {
