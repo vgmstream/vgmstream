5c52b2dee7accbc138edbf11539db8fbf0eac282
bnnm <bananaman255@gmail.com>
2025-08-16 20:05:52 +0200

    Fix some broken .bank

--- src/meta/fsb5_fev.c
+++ src/meta/fsb5_fev.c
@@ -9,8 +9,6 @@ static int get_subsongs(STREAMFILE* sf, uint32_t fsb5_offset, uint32_t fsb5_size
 VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
     VGMSTREAM* vgmstream = NULL;
     STREAMFILE* temp_sf = NULL;
-    off_t subfile_offset, chunk_offset, bank_offset, offset;
-    size_t subfile_size, bank_size;
     uint32_t version = 0;
     int fsb5_pos, fsb5_subsong;
     int total_subsongs, target_subsong = sf->stream_index;
@@ -31,43 +29,49 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
      * form a .bank, which is the format we support here (regular .fev is complex and not very interesting).
      * Format is RIFF with FMT (main), LIST (config) and SND (FSB5 data), we want the FSB5 offset inside LIST */
 
-    if (!find_chunk_le(sf, 0x4C495354, 0x0c, 0, &chunk_offset,NULL)) /* "LIST" */
-        goto fail;
-    if (read_u32be(chunk_offset+0x00,sf) != 0x50524F4A || /* "PROJ" */
-        read_u32be(chunk_offset+0x04,sf) != 0x424E4B49)   /* "BNKI" */
-        goto fail; /* event .fev has "OBCT" instead of "BNKI" (which can also be empty) */
+    off_t chunk_offset;
+    if (!find_chunk_le(sf, get_id32be("LIST"), 0x0c, 0, &chunk_offset,NULL))
+        return NULL;
+    if (read_u32be(chunk_offset+0x00,sf) != get_id32be("PROJ") ||
+        read_u32be(chunk_offset+0x04,sf) != get_id32be("BNKI"))
+        return NULL; /* event .fev has "OBCT" instead of "BNKI" (which can also be empty) */
 
 
     /* inside BNKI is a bunch of LISTs each with event subchunks and somewhere the FSB5 offsets */
-    bank_offset = 0;
-    offset = chunk_offset + 0x04;
-    while (bank_offset == 0 && offset < get_streamfile_size(sf)) {
+    uint32_t sndh_offset = 0, sndh_size = 0;
+    //uint32_t pefx_offset = 0, pefx_size = 0;
+    uint32_t offset = chunk_offset + 0x04;
+    while (sndh_offset == 0 && offset < get_streamfile_size(sf)) {
         uint32_t chunk_type = read_u32be(offset + 0x00,sf);
         uint32_t chunk_size = read_u32le(offset + 0x04,sf);
         offset += 0x08;
 
         if (chunk_type == 0xFFFFFFFF || chunk_size == 0xFFFFFFFF)
-            goto fail;
+            return NULL;
 
+        //;VGM_LOG("FSB5 FEV: chunk=%x, offset=%x, size=%x\n", chunk_type, offset - 0x08, chunk_size);
         switch(chunk_type) {
             case 0x4C495354: /* "LIST" */
                 // "SNDH" (usually v0x28 but also in Fall Guys's BNK_Music_RoundReveal)
                 if (read_u32be(offset + 0x04, sf) == get_id32be("SNDH")) {
-                    bank_offset = offset + 0x0c;
-                    bank_size = read_s32le(offset + 0x08,sf);
+                    sndh_offset = offset + 0x0c;
+                    sndh_size = read_s32le(offset + 0x08,sf);
                 }
-
+#if 0
                 // multiple CrankcaseAudio REV ADM3 [Rennsport (PC)]
+                // inside PEFX there are multiple LIST w/ LCNT + PEFF + PEFB > ADM3 (each with multiple subsongs).
+                // can be set found empty (no LCNT) with SNDH, so possibly both FSB5 and ADM3 could coexist
                 if (read_u32be(offset + 0x00, sf) == get_id32be("PEFX")) {
-                    bank_offset = offset;
-                    bank_size = chunk_size;
+                    pefx_offset = offset + 0x0c;
+                    pefx_size = chunk_size;
                 }
+#endif
                 break;
 
             case 0x534E4448: /* "SNDH" */
-                bank_offset = offset;
-                bank_size = chunk_size;
-                if (bank_size == 0) {
+                sndh_offset = offset;
+                sndh_size = chunk_size;
+                if (sndh_size == 0) {
                     vgm_logi("FSB: bank has no subsongs (ignore)\n");
                     goto fail;
                 }
@@ -80,9 +84,11 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
         offset += chunk_size;
     }
 
-    if (bank_offset == 0)
-        goto fail;
+    if (sndh_offset == 0)
+        return NULL;
+    //;VGM_LOG("FSB5 FEV: offset=%x, size=%x\n", sndh_offset, sndh_size);
 
+    uint32_t subfile_offset, subfile_size;
     {
         /* known bank versions:
          * 0x28: Transistor (iOS) [~2015]
@@ -94,10 +100,10 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
          * 0x7D: Fall Guys (PC) [~2020]
          * 0x84: SCP Unity (PC) [~2020]
          * 0x86: Hades (Switch) [~2020] */
-        size_t entry_size = version <= 0x28 ? 0x04 : 0x08;
+        uint32_t entry_size = version <= 0x28 ? 0x04 : 0x08;
 
         /* 0x00: unknown (chunk version? ex LE: 0x00080003, 0x00080005) */
-        int banks = (bank_size - 0x04) / entry_size;
+        int banks = (sndh_size - 0x04) / entry_size;
 
         /* multiple banks is possible but rare [Hades (Switch), Guacamelee 2 (Switch)],
          * must map bank (global) subsong to FSB (internal) subsong */
@@ -109,8 +115,8 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
         total_subsongs = 0;
         for (int i = 0; i < banks; i++) {
             //TODO: fsb5_size fails for v0x28< + encrypted, but only used with multibanks = unlikely
-            uint32_t fsb5_offset  = read_u32le(bank_offset + 0x04 + entry_size*i + 0x00,sf);
-            uint32_t fsb5_size   = read_u32le(bank_offset + 0x08 + entry_size*i,sf);
+            uint32_t fsb5_offset  = read_u32le(sndh_offset + 0x04 + entry_size * i + 0x00,sf);
+            uint32_t fsb5_size    = read_u32le(sndh_offset + 0x08 + entry_size * i,sf);
             int fsb5_subsongs = get_subsongs(sf, fsb5_offset, fsb5_size);
             if (!fsb5_subsongs) {
                 vgm_logi("FSB: couldn't load bank %i at %x (encrypted?)\n", i, fsb5_offset);
@@ -129,7 +135,7 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
             goto fail;
 
         if (version <= 0x28) {
-            subfile_offset  = read_u32le(bank_offset+0x04 + entry_size*fsb5_pos,sf);
+            subfile_offset  = read_u32le(sndh_offset+0x04 + entry_size*fsb5_pos,sf);
             subfile_size    = /* meh */
                 read_u32le(subfile_offset + 0x0C,sf) + 
                 read_u32le(subfile_offset + 0x10,sf) + 
@@ -137,12 +143,12 @@ VGMSTREAM* init_vgmstream_fsb5_fev_bank(STREAMFILE* sf) {
                 0x3C;
         }
         else {
-            subfile_offset  = read_u32le(bank_offset+0x04 + entry_size*fsb5_pos,sf);
-            subfile_size    = read_u32le(bank_offset+0x08 + entry_size*fsb5_pos,sf);
+            subfile_offset  = read_u32le(sndh_offset+0x04 + entry_size*fsb5_pos,sf);
+            subfile_size    = read_u32le(sndh_offset+0x08 + entry_size*fsb5_pos,sf);
         }
     }
 
-    //;VGM_LOG("FSB5 FEV: offset=%lx, size=%x\n", subfile_offset,subfile_size);
+    ;VGM_LOG("FSB5 FEV: offset=%x, size=%x\n", subfile_offset, subfile_size);
 
     temp_sf = setup_subfile_streamfile(sf, subfile_offset,subfile_size, "fsb");
     if (!temp_sf) goto fail;
