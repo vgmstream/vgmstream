9efbea62211b11f379fa205ec10a4964c4a011d8
bnnm <bananaman255@gmail.com>
2025-12-20 11:38:48 +0100

    cleanup: swap .isb+ish to .ish+isb

--- src/meta/ish_isd.c
+++ src/meta/ish_isd.c
@@ -1,60 +1,56 @@
 #include "meta.h"
 #include "../coding/coding.h"
+#include "../util/meta_utils.h"
 
-/* ISH+ISD - from various games [Chaos Field (GC), Pokemon XD - Gale of Darkness (GC)] */
-VGMSTREAM * init_vgmstream_ish_isd(STREAMFILE *streamFile) {
-    VGMSTREAM * vgmstream = NULL;
-    STREAMFILE * streamHeader = NULL;
-    off_t start_offset;
-    int channel_count, loop_flag;
 
+/* ISH+ISD - from various games [Chaos Field (GC), Pokemon XD: Gale of Darkness (GC)] */
+VGMSTREAM* init_vgmstream_ish_isd(STREAMFILE* sf) {
+    VGMSTREAM* vgmstream = NULL;
 
     /* checks */
-    if (!check_extensions(streamFile, "isd"))
-        goto fail;
+    if (!is_id32be(0x00,sf, "I_SF"))
+        return NULL;
+    if (!check_extensions(sf, "ish"))
+        return NULL;
 
-    streamHeader = open_streamfile_by_ext(streamFile,"ish");
-    if (!streamHeader) goto fail;
+    meta_header_t h = {0};
+    h.meta = meta_ISH_ISD,
 
-    if (read_32bitBE(0x00,streamHeader) != 0x495F5346) /* "I_SF" */
-        goto fail;
+    //04: tool date in hex? (0x2004 01 09)
+    h.sample_rate   = read_s32be(0x08,sf);
+    h.num_samples   = read_s32be(0x0C,sf);
+    //10: stream size
+    h.channels      = read_s32be(0x14,sf);
+    h.interleave    = read_u32be(0x18,sf);
+    h.loop_flag     = read_s32be(0x1C,sf) != 0;
+    h.loop_start    = read_s32be(0x20,sf);
+    h.loop_end      = read_s32be(0x24,sf);
+    //28: padding
+    h.coefs_offset  = 0x40;
+    h.coefs_spacing = 0x40;
+    h.big_endian = true;
 
-    channel_count = read_32bitBE(0x14,streamHeader);
-    loop_flag = (read_32bitBE(0x1C,streamHeader) != 0);
-    start_offset = 0;
+    //TODO: loop start may be set mid-frame, which bytes_to_samples doesn't handle
+    //h.loop_start = dsp_bytes_to_samples(h.loop_start, h.channels);
+    //h.loop_end = dsp_bytes_to_samples(h.loop_end, h.channels);
+    h.loop_start = h.loop_start * 14 / 0x08 / h.channels;
+    h.loop_end = h.loop_end * 14 / 0x08 / h.channels;
 
+    h.stream_offset = 0x00;
 
-    /* build the VGMSTREAM */
-    vgmstream = allocate_vgmstream(channel_count,loop_flag);
-    if (!vgmstream) goto fail;
+    h.sf_head = sf;
+    h.sf_body = open_streamfile_by_ext(sf,"isd");
+    if (!h.sf_body) goto fail;
 
-    vgmstream->sample_rate = read_32bitBE(0x08,streamHeader);
-    vgmstream->num_samples = read_32bitBE(0x0C,streamHeader);
-    if (loop_flag) {
-        vgmstream->loop_start_sample = read_32bitBE(0x20,streamHeader)*14 / 0x08 /channel_count;
-        vgmstream->loop_end_sample = read_32bitBE(0x24,streamHeader)*14 / 0x08 / channel_count;
-    }
+    h.coding = coding_NGC_DSP;
+    h.layout = layout_interleave;
+    h.open_stream = true;
 
-    vgmstream->meta_type = meta_ISH_ISD;
-    vgmstream->coding_type = coding_NGC_DSP;
-    if (channel_count == 1) {
-        vgmstream->layout_type = layout_none;
-    } else {
-        vgmstream->layout_type = layout_interleave;
-        vgmstream->interleave_block_size = read_32bitBE(0x18,streamHeader);
-    }
-
-    dsp_read_coefs_be(vgmstream,streamHeader,0x40,0x40);
-
-
-    if (!vgmstream_open_stream(vgmstream,streamFile,start_offset))
-        goto fail;
-
-    close_streamfile(streamHeader);
+    vgmstream = alloc_metastream(&h);
+    close_streamfile(h.sf_body);
     return vgmstream;
-
 fail:
-    close_streamfile(streamHeader);
+    close_streamfile(h.sf_body);
     close_vgmstream(vgmstream);
     return NULL;
 }
