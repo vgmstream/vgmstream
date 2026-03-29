790f86dbdc3ec9383804477bde8590471596734e
bnnm <bananaman255@gmail.com>
2026-03-29 23:18:56 +0200

    dkt: improve validation

--- src/meta/ngc_adpdtk.c
+++ src/meta/ngc_adpdtk.c
@@ -15,25 +15,34 @@ VGMSTREAM* init_vgmstream_dtk(STREAMFILE* sf) {
      * .trk: Bloody Roar: Primal Fury (GC)
      * .wav/lwav: Alien Hominid (GC) */
     if (!check_extensions(sf,"dtk,adp,trk,wav,lwav"))
-        goto fail;
+        return NULL;
 
     /* check valid frames as files have no header, and .adp/wav are common */
     {
-        int i, blanks = 0;
-        for (i = 0; i < 10; i++) { /* try a bunch of frames */
-            /* header 0x00/01 are repeated in 0x02/03 (for error correction?),
-             * could also test header values (upper nibble should be 0..3, and lower nibble 0..C) */
-            if (read_u8(0x00 + i*0x20,sf) != read_u8(0x02 + i*0x20,sf) ||
-                read_u8(0x01 + i*0x20,sf) != read_u8(0x03 + i*0x20,sf))
-                goto fail;
+        int max_frames = 10; //arbitrary max
+        int blanks = 0;
+        uint32_t offset = 0x00;
+        for (int i = 0; i < max_frames; i++) {
+            // upper nibble should be 0..3, and lower nibble 0..C
+            uint8_t frame_header = read_u8(offset + 0x00,sf);
+            int index = (frame_header >> 4) & 0xf;
+            int shift = (frame_header >> 0) & 0xf;
+            if(index > 4 || shift > 0x0c)
+                return NULL;
 
-            /* silent frame headers are almost always 0x0C, save a few uncommon tracks [Wave Race Blue Storm (GC), 1080 Silver Storm (GC)] */
-            if (read_u16be(0x00 + i*0x20,sf) == 0x00) 
+            // header 0x00/01 are repeated in 0x02/03 (for error correction?)
+            if (read_u8(offset + 0x00,sf) != read_u8(offset + 0x02,sf) ||
+                read_u8(offset + 0x01,sf) != read_u8(offset + 0x03,sf))
+                return NULL;
+
+            // silent frame headers are almost always 0x0C, save a few uncommon tracks [Wave Race Blue Storm (GC), 1080 Silver Storm (GC)]
+            if (read_u16be(offset + 0x00,sf) == 0x00) 
                 blanks++;
+            offset += 0x20;
         }
 
         if (blanks > 3)
-            goto fail;
+            return NULL;
     }
 
     /* DTK (Disc Track) are DVD hardware-decoded streams, always stereo and no loop.
@@ -49,7 +58,7 @@ VGMSTREAM* init_vgmstream_dtk(STREAMFILE* sf) {
     if (!vgmstream) goto fail;
 
     vgmstream->num_samples = get_streamfile_size(sf) / 0x20 * 28;
-    vgmstream->sample_rate = 48000; /* due to a GC hardware defect this may be closer to 48043 */
+    vgmstream->sample_rate = 48000; // due to a GC hardware defect this may be closer to 48043
     vgmstream->coding_type = coding_NGC_DTK;
     vgmstream->layout_type = layout_none;
     vgmstream->meta_type = meta_DTK;
