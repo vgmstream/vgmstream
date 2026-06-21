3e88276f3bca026be742918b2ae88eec9c7f6cbc
bnnm <bananaman255@gmail.com>
2026-06-21 13:19:21 +0200

    cleanup: split wmsf and 2msf

--- src/meta/2msf.c
+++ src/meta/2msf.c
@@ -1,27 +1,28 @@
 #include "meta.h"
 #include "../coding/coding.h"
 
-/* WMSF - Banpresto MSFx wrapper [Dai-2-Ji Super Robot Taisen OG: The Moon Dwellers (PS3)] */
-VGMSTREAM* init_vgmstream_wmsf(STREAMFILE* sf) {
+/* 2MSF - Banpresto RIFF wrapper [Dai-2-Ji Super Robot Taisen OG: The Moon Dwellers (PS4)] */
+VGMSTREAM* init_vgmstream_2msf(STREAMFILE* sf) {
     VGMSTREAM* vgmstream = NULL;
     STREAMFILE* temp_sf = NULL;
 
     /* checks */
-    if (!is_id32be(0x00,sf,"WMSF"))
+    if (!is_id32be(0x00,sf,"2MSF"))
         return NULL;
-    if (!check_extensions(sf,"msf"))
+    if (!check_extensions(sf,"at9"))
         return NULL;
     // 0x04: size
     // 0x08: flags?
     // 0x0c: null?
-    
-    uint32_t subfile_offset = 0x10;
+    // 0x10: 0x01? (BE values even though RIFF is LE)
+
+    uint32_t subfile_offset = 0x14;
     uint32_t subfile_size = get_streamfile_size(sf) - subfile_offset;
 
     temp_sf = setup_subfile_streamfile(sf, subfile_offset, subfile_size, NULL);
     if (!temp_sf) goto fail;
 
-    vgmstream = init_vgmstream_msf(temp_sf);
+    vgmstream = init_vgmstream_riff(temp_sf);
     if (!vgmstream) goto fail;
 
     close_streamfile(temp_sf);
